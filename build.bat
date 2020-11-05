207b7686dc1b3efd8182831e385946ff8520886f
xavier dupré <xavier.dupre@gmail.com>
2020-11-05 18:04:35 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
+@echo off
 REM Copy this file to mybuild.bat and make the changes as you needs
 
 REM call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+cmake --version
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir .\out\Windows\ 2>NUL
 cd out\Windows
