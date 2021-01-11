03d951fe1449afa779169b32ed2acce44ff9247b
Wenbing Li <wenbingl@outlook.com>
2021-01-10 18:02:39 -0800

    fixing on Windows platform

--- setup.py
+++ setup.py
@@ -6,19 +6,20 @@
 # license information.
 ###########################################################################
 
-from distutils.core import setup
 from setuptools.command.build_ext import build_ext as _build_ext
 from setuptools.command.develop import develop as _develop
 from setuptools.command.build_py import build_py as _build_py
+from distutils.core import setup
 from contextlib import contextmanager
 
 import os
+import sys
 import setuptools
 import pathlib
+import subprocess
 
 
 TOP_DIR = os.path.dirname(__file__)
-CMAKE_BUILD_DIR = os.path.join(TOP_DIR, '.setuptools-cmake-build')
 
 
 @contextmanager
@@ -31,11 +32,24 @@ def chdir(path):
         os.chdir(orig_path)
 
 
-class BuildCMakeExt(_build_ext):
+def load_msvcvar():
+    if os.environ.get('vcvars'):
+        stdout, _ = subprocess.Popen([
+            'cmd', '/q', '/c', '(%vcvars% & set)'],
+            stdout=subprocess.PIPE, shell=True, universal_newlines=True).communicate()
+        for line in stdout.splitlines():
+            kv_pair = line.split('=')
+            if len(kv_pair) == 2:
+                os.environ[kv_pair[0]] = kv_pair[1]
+    else:
+        import shutil
+        if shutil.which('cmake') is None:
+            raise SystemExit(
+                "Cannot find cmake in the executable path, " +
+                "please install one or specify the environement variable VCVARS to the path of VS vcvars64.bat.")
 
-    def initialize_options(self):
-        super().initialize_options()
-        self.build_base = CMAKE_BUILD_DIR
+
+class BuildCMakeExt(_build_ext):
 
     def run(self):
         """
@@ -46,17 +60,21 @@ class BuildCMakeExt(_build_ext):
                 self.build_cmake(extension)
 
     def build_cmake(self, extension):
-        cwd = pathlib.Path().absolute()
+        project_dir = pathlib.Path().absolute()
         build_temp = pathlib.Path(self.build_temp)
         build_temp.mkdir(parents=True, exist_ok=True)
         ext_fullpath = pathlib.Path(self.get_ext_fullpath(extension.name))
 
-        config = 'Debug' if self.debug else 'Release'
+        config = 'RelWithDebInfo' if self.debug else 'Release'
         cmake_args = [
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(ext_fullpath.parent.absolute()),
             '-DENABLE_PYTHON=ON',
-            '-DOCOS_EXTENTION_NAME=' + pathlib.Path(self.get_ext_filename(extension.name)).name
+            '-DOCOS_EXTENTION_NAME=' + pathlib.Path(self.get_ext_filename(extension.name)).name,
+            '-DCMAKE_BUILD_TYPE=' + config
         ]
+
+        if self.debug:
+            cmake_args += ['-DCC_OPTIMIZE=OFF']
 
         build_args = [
             '--config', config,
@@ -64,9 +82,12 @@ class BuildCMakeExt(_build_ext):
         ]
 
         with chdir(build_temp):
-            self.spawn(['cmake', str(cwd)] + cmake_args)
+            self.spawn(['cmake', str(project_dir)] + cmake_args)
             if not self.dry_run:
                 self.spawn(['cmake', '--build', '.'] + build_args)
+
+        if sys.platform == "win32":
+            self.copy_file(build_temp / config / 'ortcustomops.dll', self.get_ext_filename(extension.name))
 
 
 class BuildPy(_build_py):
@@ -95,6 +116,10 @@ def read_version():
         if len(line) > 0:
             version_str = line[0].split('=')[1].strip('" ')
     return version_str
+
+
+if sys.platform == "win32":
+    load_msvcvar()
 
 
 ext_modules = [
