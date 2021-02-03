016f28bb380312b730dd94e229021ebf7f7dd00a
xavier dupré <xavier.dupre@gmail.com>
2021-02-03 15:22:01 +0100

    fix potential wrong pointer
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- ocos/kernels/kernels.cc
+++ ocos/kernels/kernels.cc
@@ -13,14 +13,16 @@ bool BaseKernel::HasAttribute(const char* name) const {
   // Crashes here.
   OrtStatus* status = api_.KernelInfoGetAttribute_string(info_, name, nullptr, &size);
   auto r = api_.GetErrorCode(status);
-  api_.ReleaseStatus(status);
   bool has = (r == ORT_INVALID_ARGUMENT) || (r == ORT_OK);
   if (has) {
+    api_.ReleaseStatus(status);
     return has;
   }
   const char* error = api_.GetErrorMessage(status);
   if (strstr(error, "No attribute") == error) {
+    api_.ReleaseStatus(status);
     return false;
   }
+  api_.ReleaseStatus(status);
   return true;
 }
