598abe5c8ee27d5aab949e9c26db9a187617dcb7
xavier dupré <xavier.dupre@gmail.com>
2021-04-09 00:51:47 +0200

    fix build
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- ci_build/azure-pipelines/install_cmake.sh
+++ ci_build/azure-pipelines/install_cmake.sh
@@ -1,6 +1,4 @@
 mkdir _install_
 cd _install_
-wget https://github.com/Kitware/CMake/releases/download/v3.20.1/cmake-3.20.1-linux-x86_64.sh
-/bin/ls
+wget https://github.com/Kitware/CMake/releases/download/v3.20.1/cmake-3.20.1-linux-x86_64.sh -v
 bash cmake-3.20.1-linux-x86_64.sh --skip-license
-/bin/ls
