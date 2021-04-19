93b693fa4f1fc77ce04eaa600795be414103e846
Ze Tao <zetao@microsoft.com>
2021-04-19 20:03:20 +0800

    union build

---
+++ ci_build/onnxruntime_integration/build_with_onnxruntime.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIR=onnxruntime_customops_integration
+
+if [ ! -d $BASE_DIR ]
+then	
+    mkdir $BASE_DIR
+fi
+cd $BASE_DIR
+
+git clone https://github.com/microsoft/onnxruntime.git
+cd onnxruntime
+
+# there is no stable version including webassembly
+git checkout 21c282ed
+cd cmake/external
+git clone git@github.com:microsoft/ort-customops.git
+
+cd ../..
+
+cp cmake/external/ort-customops/test/data/custom_op_string_lower.onnx onnxruntime/test/testdata
+git apply cmake/external/ort-customops/ci_build/onnxruntime_integration/onnxruntime.patch
+
+#get ready and begin building
+cd ..
+if [ ! -d build ]
+then
+    mkdir build
+fi
+python3 onnxruntime/tools/ci_build/build.py --build_dir build "$@"
+
+
