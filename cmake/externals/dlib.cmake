219332ce893e96a3eb155ac117141d8a0a75d506
Wenbing Li <wenbingl@outlook.com>
2021-05-04 14:26:38 -0700

    enable dlib library on Win32 build

--- cmake/externals/dlib.cmake
+++ cmake/externals/dlib.cmake
@@ -3,8 +3,12 @@ FetchContent_Declare(dlib
     GIT_TAG        v19.22
 )
 
-FetchContent_GetProperties(dlib)
-if(NOT dlib_POPULATED)
-  # Fetch the content using previously declared details
-  FetchContent_Populate(dlib)
+if (WIN32)
+  FetchContent_MakeAvailable(dlib)
+else()
+  FetchContent_GetProperties(dlib)
+  if(NOT dlib_POPULATED)
+    # Fetch the content using previously declared details
+    FetchContent_Populate(dlib)
+  endif()
 endif()
