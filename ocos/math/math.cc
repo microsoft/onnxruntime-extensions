aecc9f367ad6b3cba974c2f5f2ce189a5ab729b6
Wenbing Li <wenbingl@outlook.com>
2021-05-04 14:39:30 -0700

    rename ocos to operators

--- ocos/math/math.cc
+++
@@ -1,7 +0,0 @@
-#include "ocos.h"
-#include "negpos.hpp"
-#include "inverse.hpp"
-
-template const OrtCustomOp** LoadCustomOpClasses<CustomOpNegPos, CustomOpInverse>();
-
-FxLoadCustomOpFactory LoadCustomOpClasses_Math = &LoadCustomOpClasses<CustomOpNegPos, CustomOpInverse>;
