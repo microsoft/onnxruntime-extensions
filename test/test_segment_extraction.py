c4970ce2d5d1f6a8791b19b6178411c118b0011d
Toudsour <Toudsour@gmail.com>
2021-08-28 02:48:43 +0800

    fix bug

--- test/test_segment_extraction.py
+++ test/test_segment_extraction.py
@@ -16,21 +16,21 @@ def _run_segment_extraction(input, expect_position, expect_value):
     np.testing.assert_array_equal(value, expect_value)
 
 
-class TestBlingFireSentenceBreaker(unittest.TestCase):
+class TestSegmentExtraction(unittest.TestCase):
 
     def test_text_to_case1(self):
         inputs = np.array([0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3], dtype=np.int64)
-        position = [[2, 3], [4, 6], [7, 10]]
+        position = [[2, 4], [4,7], [7, 11]]
         value = [1, 2, 3]
         _run_segment_extraction(inputs, position, value)
 
         inputs = np.array([1, 1, 0, 0, 2, 2, 2, 3, 3, 3, 0, 5], dtype=np.int64)
-        position = [[0, 1], [4, 6], [7, 9], [11, 11]]
+        position = [[0, 2], [4, 7], [7, 10], [11, 12]]
         value = [1, 2, 3, 5]
         _run_segment_extraction(inputs, position, value)
 
         inputs = np.array([1, 2, 4, 5], dtype=np.int64)
-        position = [[0, 0], [1, 1], [2, 2], [3, 3]]
+        position = [[0, 1], [1, 2], [2, 3], [3, 4]]
         value = [1, 2, 4, 5]
         _run_segment_extraction(inputs, position, value)
 
