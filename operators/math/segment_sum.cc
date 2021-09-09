6d7a8659136f446d3db249dde61217b32b3ad2e6
Zuwei Zhao <4123666+Zuwei-Zhao@users.noreply.github.com>
2021-09-08 19:21:40 -0500

    Disable c++ exceptions in onnxruntime-extensions. (#143)
    * Disable c++ exceptions in onnxruntime-extensions.
    
    * Remove cxx flags for extensions.
    
    * Remove redundant lines.
    
    Co-authored-by: Zuwei Zhao <zuzhao@microsoft.com>

--- operators/math/segment_sum.cc
+++ operators/math/segment_sum.cc
@@ -15,13 +15,13 @@ void KernelSegmentSum_Compute(Ort::CustomOpApi& ort_, OrtKernelContext* context)
   OrtTensorDimensions dim_data(ort_, data);
   OrtTensorDimensions dim_seg(ort_, segment_ids);
   if (dim_data.size() == 0 || dim_seg.size() == 0)
-    throw std::runtime_error("Both inputs cannot be empty.");
+    ORT_CXX_API_THROW("Both inputs cannot be empty.", ORT_INVALID_ARGUMENT);
   if (dim_seg.size() != 1)
-    throw std::runtime_error("segment_ids must a single tensor");
+    ORT_CXX_API_THROW("segment_ids must a single tensor", ORT_INVALID_ARGUMENT);
   if (dim_data[0] != dim_seg[0])
-    throw std::runtime_error(MakeString(
+    ORT_CXX_API_THROW(MakeString(
         "First dimensions of data and segment_ids should be the same, data shape: ", dim_data.GetDims(),
-        " segment_ids shape: ", dim_seg.GetDims()));
+        " segment_ids shape: ", dim_seg.GetDims()), ORT_INVALID_ARGUMENT);
 
   int64_t last_seg = p_segment_ids[dim_seg[0] - 1];
   OrtTensorDimensions dim_out = dim_data;
@@ -42,9 +42,9 @@ void KernelSegmentSum_Compute(Ort::CustomOpApi& ort_, OrtKernelContext* context)
   const int64_t* p_seg = p_segment_ids;
   for (; begin != end; ++p_seg) {
     if ((p_seg != p_segment_ids) && (*p_seg != *(p_seg - 1)) && (*p_seg != *(p_seg - 1) + 1))
-      throw std::runtime_error(MakeString("segment_ids must be increasing but found ",
+      ORT_CXX_API_THROW(MakeString("segment_ids must be increasing but found ",
                                           *(p_seg - 1), " and ", *p_seg, " at position ",
-                                          std::distance(p_segment_ids, p_seg), "."));
+                                          std::distance(p_segment_ids, p_seg), "."), ORT_INVALID_ARGUMENT);
     p_out = p_output + *p_seg * in_stride;
     p_out_end = p_out + in_stride;
     for (; p_out != p_out_end; ++p_out, ++begin)
@@ -86,6 +86,6 @@ ONNXTensorElementDataType CustomOpSegmentSum::GetInputType(size_t index) const {
     case 1:
       return ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64;
     default:
-      throw std::runtime_error("Operator SegmentSum has 2 inputs.");
+      ORT_CXX_API_THROW("Operator SegmentSum has 2 inputs.", ORT_INVALID_ARGUMENT);
   }
 };
