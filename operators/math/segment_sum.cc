3542434876449d1402c392dc084b47ec8f464e61
Mojimi <toudsour@gmail.com>
2021-09-11 01:23:35 +0800

    Fix op selection (#147)
    * fix segement_extraction
    
    * update
    
    Co-authored-by: Ze Tao <zetao@microsoft.com>

--- operators/math/segment_sum.cc
+++ operators/math/segment_sum.cc
@@ -15,13 +15,13 @@ void KernelSegmentSum_Compute(Ort::CustomOpApi& ort_, OrtKernelContext* context)
   OrtTensorDimensions dim_data(ort_, data);
   OrtTensorDimensions dim_seg(ort_, segment_ids);
   if (dim_data.size() == 0 || dim_seg.size() == 0)
-    ORT_CXX_API_THROW("Both inputs cannot be empty.", ORT_INVALID_ARGUMENT);
+    ORT_CXX_API_THROW("Both inputs cannot be empty.", ORT_INVALID_GRAPH);
   if (dim_seg.size() != 1)
-    ORT_CXX_API_THROW("segment_ids must a single tensor", ORT_INVALID_ARGUMENT);
+    ORT_CXX_API_THROW("segment_ids must a single tensor", ORT_INVALID_GRAPH);
   if (dim_data[0] != dim_seg[0])
     ORT_CXX_API_THROW(MakeString(
         "First dimensions of data and segment_ids should be the same, data shape: ", dim_data.GetDims(),
-        " segment_ids shape: ", dim_seg.GetDims()), ORT_INVALID_ARGUMENT);
+        " segment_ids shape: ", dim_seg.GetDims()), ORT_INVALID_GRAPH);
 
   int64_t last_seg = p_segment_ids[dim_seg[0] - 1];
   OrtTensorDimensions dim_out = dim_data;
@@ -44,7 +44,7 @@ void KernelSegmentSum_Compute(Ort::CustomOpApi& ort_, OrtKernelContext* context)
     if ((p_seg != p_segment_ids) && (*p_seg != *(p_seg - 1)) && (*p_seg != *(p_seg - 1) + 1))
       ORT_CXX_API_THROW(MakeString("segment_ids must be increasing but found ",
                                           *(p_seg - 1), " and ", *p_seg, " at position ",
-                                          std::distance(p_segment_ids, p_seg), "."), ORT_INVALID_ARGUMENT);
+                                          std::distance(p_segment_ids, p_seg), "."), ORT_RUNTIME_EXCEPTION);
     p_out = p_output + *p_seg * in_stride;
     p_out_end = p_out + in_stride;
     for (; p_out != p_out_end; ++p_out, ++begin)
