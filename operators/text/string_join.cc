05f7ded825348f403f1644b04a4cd5d3406969fd
Zuwei Zhao <4123666+Zuwei-Zhao@users.noreply.github.com>
2021-10-27 20:21:16 +0800

    Add check for empty input in StringJoin operator and fix empty string input error in BlingFire sentence breaker. (#175)
    * Add test cases and fix empty string error in BlingFire sentence breaker.
    
    * Throw error if input text to join is empty array.
    
    * Fix scalar support and access violation.
    
    * Resolve comments.
    
    * Resolve comments.
    
    Co-authored-by: Zuwei Zhao <zuzhao@microsoft.com>

--- operators/text/string_join.cc
+++ operators/text/string_join.cc
@@ -17,16 +17,22 @@ void KernelStringJoin::Compute(OrtKernelContext* context) {
   GetTensorMutableDataString(api_, ort_, context, input_X, X);
   GetTensorMutableDataString(api_, ort_, context, input_sep, sep);
 
-  // Setup output
+  // Check input
   OrtTensorDimensions dimensions_sep(ort_, input_sep);
   if (dimensions_sep.size() != 1 || dimensions_sep[0] != 1)
-    ORT_CXX_API_THROW("Input 2 is the separator, it has 1 element.", ORT_INVALID_ARGUMENT);
+    ORT_CXX_API_THROW("Input 2 is the separator, it should have 1 element.", ORT_INVALID_ARGUMENT);
   OrtTensorDimensions dimensions_axis(ort_, input_axis);
   if (dimensions_axis.size() != 1 || dimensions_axis[0] != 1)
-    ORT_CXX_API_THROW("Input 3 is the axis, it has 1 element.", ORT_INVALID_ARGUMENT);
+    ORT_CXX_API_THROW("Input 3 is the axis, it should have 1 element.", ORT_INVALID_ARGUMENT);
   OrtTensorDimensions dimensions(ort_, input_X);
-  if (*axis < 0 || *axis >= dimensions.size())
-    ORT_CXX_API_THROW(MakeString("axis must be positive and smaller than the number of dimension but it is ", *axis), ORT_INVALID_ARGUMENT);
+  if (dimensions.size() == 0) {
+    // dimensions size 0 means input 1 is scalar, input 1 must have 1 element. See issue: https://github.com/onnx/onnx/issues/3724
+    if (X.size() != 1)
+      ORT_CXX_API_THROW(MakeString("Input 1's dimensions size is 0 (scalar), it must has 1 element but it has ", X.size()), ORT_INVALID_ARGUMENT);
+  } else {
+    if (*axis < 0 || *axis >= dimensions.size())
+      ORT_CXX_API_THROW(MakeString("axis must be positive and smaller than the number of dimension but it is ", *axis), ORT_INVALID_ARGUMENT);
+  }
 
   std::vector<int64_t> dimensions_out(dimensions.size() > 1 ? dimensions.size() - 1 : 1);
   if (dimensions.size() > 1) {
@@ -45,27 +51,38 @@ void KernelStringJoin::Compute(OrtKernelContext* context) {
   ort_.ReleaseTensorTypeAndShapeInfo(output_info);
   std::vector<std::string> out(size);
 
-  // Do computation
-  int64_t h = 1;
-  for (size_t i = *axis + 1; i < dimensions.size(); ++i) {
-    h *= dimensions[i];
-  }
-  int64_t left_part = size / h;
-  int64_t right_part = size / left_part;
-  int64_t n_red = dimensions[*axis] - 1;
-  int64_t inc = right_part * (n_red + 1);
-  int64_t pos = 0;
-  for (int64_t li = 0; li < left_part; ++li) {
-    for (int64_t ri = 0; ri < right_part; ++ri, ++pos) {
-      std::ostringstream st;
-      int64_t index = ri + li * inc;
-      for (int64_t j = 0; j < n_red; ++j, index += h) {
-        st << X[index] << sep[0];
+  if (dimensions.size() > 0) {
+    if (X.size() > 0) {
+      // Do computation
+      int64_t h = 1;
+      for (size_t i = *axis + 1; i < dimensions.size(); ++i) {
+        h *= dimensions[i];
       }
-      st << X[index];
-      out[pos] = st.str();
+      int64_t left_part = size / h;
+      int64_t right_part = size / left_part;
+      int64_t n_red = dimensions[*axis] - 1;
+      int64_t inc = right_part * (n_red + 1);
+      int64_t pos = 0;
+      for (int64_t li = 0; li < left_part; ++li) {
+        for (int64_t ri = 0; ri < right_part; ++ri, ++pos) {
+          std::ostringstream st;
+          int64_t index = ri + li * inc;
+          for (int64_t j = 0; j < n_red; ++j, index += h) {
+            st << X[index] << sep[0];
+          }
+          st << X[index];
+          out[pos] = st.str();
+        }
+      }
+    } else {
+      // for input 1 contains 0 elements, output joined string is empty string
+      out[0] = "";
     }
+  } else {
+    // for input 1 (scalar) which has 1 element, output joined string is input string itself. See issue: https://github.com/onnx/onnx/issues/3724
+    out[0] = X[0];
   }
+
   FillTensorDataString(api_, ort_, context, out, output);
 }
 
