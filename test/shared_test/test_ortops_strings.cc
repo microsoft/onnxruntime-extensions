e1672aeeb76b9d793a83dd6603801e6c1ead9e01
Ze Tao <zetao@microsoft.com>
2021-10-28 10:04:03 +0800

    init

--- test/shared_test/test_ortops_strings.cc
+++ test/shared_test/test_ortops_strings.cc
@@ -265,3 +265,27 @@ TEST(string_operator, test_string_ecmaregex_replace) {
   outputs[0].values_string = {"Test+ test", "tEsT+ Test", " TEST+ test"};
   TestInference(*ort_env, model_path.c_str(), inputs, outputs, GetLibraryPath());
 }
+
+TEST(string_operator, test_string_to_vector) {
+  auto ort_env = std::make_unique<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "Default");
+
+  std::vector<TestValue> inputs(1);
+  inputs[0].name = "token_ids";
+  inputs[0].element_type = ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64;
+  inputs[0].dims = {5};
+  inputs[0].values_int64 = {0, 1, 2, 3, 4};
+
+  std::vector<TestValue> outputs(1);
+  outputs[0].name = "text";
+  outputs[0].element_type = ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING;
+  outputs[0].dims = {1};
+  outputs[0].values_string = {"a", "b", "c", "unk", "unk"};
+
+
+  std::filesystem::path model_path = __FILE__;
+  model_path = model_path.parent_path();
+  model_path /= "..";
+  model_path /= "data";
+  model_path /= "test_vector_to_string.onnx";
+  TestInference(*ort_env, model_path.c_str(), inputs, outputs, GetLibraryPath());
+}
