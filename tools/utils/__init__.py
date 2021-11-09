e60afa75513b4ab5d2b220089991fea967f13bcc
Ze Tao <zetao@microsoft.com>
2021-11-09 10:22:39 +0800

    remove uncessary file

--- tools/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .logger import get_logger
-from .platform import (is_windows, is_macOS, is_linux)
-from .run import run
