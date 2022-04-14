3626b277029472b77e3c253120f7c0324357c92c
Wenbing Li <wenbingl@outlook.com>
2022-04-13 23:58:16 -0700

    add some comments

--- tutorials/bert_e2e.py
+++ tutorials/bert_e2e.py
@@ -10,10 +10,11 @@ from transformers.convert_graph_to_onnx import convert
 model_path = Path("onnx/bert-base-cased.onnx")
 convert(framework="pt", model="bert-base-cased", output=model_path, opset=12)
 
-
+# post-processing example function
 def post_processing_forward(*pred):
-    return torch.softmax(pred[0], axis=1)
+    return torch.softmax(pred[0], axis=2)
 
+# mapping the BertTokenizer outputs into the onnx model inputs
 def mapping_token_output(_1, _2, _3):
     return _1.unsqueeze(0), _3.unsqueeze(0), _2.unsqueeze(0)
 
@@ -23,12 +24,15 @@ ort_tok = pnp.PreHuggingFaceBert(vocab_file=
                                  '../test', 'data', 'bert_basic_cased_vocab.txt'))
 onnx_model = onnx.load_model(str(model_path))
 
+# create the final onnx model which includes pre- and post- processing.
 augmented_model = pnp.export(pnp.SequentialProcessingModule(ort_tok,
                              mapping_token_output, onnx_model, post_processing_forward),
                              ["this is a test sentence."],
                              opset_version=12,
                              output_path='bert_tok_all.onnx')
 
+
+# test the augmented onnx model with raw string input.
 model_func = OrtPyFunction.from_model('bert_tok_all.onnx')
 result = model_func(test_sentence)
 print(result)
