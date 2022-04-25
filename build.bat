64d29814162b22f99359c9bf37c94d65e7436756
Wenbing Li <wenbingl@outlook.com>
2022-04-25 11:07:54 -0700

    standardize the output folder.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-ECHO Copy this file to mybuild.bat and make the changes as you needs
+ECHO Copy this file to mybuild.bat and make any changes you deem necessary
 SETLOCAL ENABLEDELAYEDEXPANSION
-
+IF DEFINED VSINSTALLDIR GOTO :VSDEV_CMD
 set VCVARS="NOT/EXISTED"
 FOR %%I in (Enterprise Professional Community BuildTools^
   ) DO IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%%I\VC\Auxiliary\Build\vcvars64.bat" (
@@ -10,8 +10,15 @@ FOR %%I in (Enterprise Professional Community BuildTools^
 IF NOT EXIST %VCVARS% GOTO :NOT_FOUND
 ECHO Found %VCVARS%
 CALL %VCVARS%
+
+:VSDEV_CMD
+set GENERATOR="Visual Studio 16 2019"
+IF "%VisualStudioVersion:~0,2%" == "16" GOTO :START_BUILD
+set GENERATOR="Visual Studio 17 2022"
+
+:START_BUILD
 mkdir .\out\Windows\ 2>NUL
-cmake -G "Visual Studio 16 2019" -A x64 %* -B out\Windows -S .
+cmake -G %GENERATOR% -A x64 %* -B out\Windows -S .
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 cmake --build out\Windows --config RelWithDebInfo
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
@@ -19,6 +26,7 @@ GOTO :EOF
 
 :NOT_FOUND
 ECHO "No Microsoft Visual Studio 2019 installation found!"
+ECHO "  Or not run from Developer Command Prompt for VS 2022"
 EXIT /B 1
 
 ENDLOCAL
