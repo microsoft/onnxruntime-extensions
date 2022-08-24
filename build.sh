483156d6ec7822135391da63c160cefb8ceac0f2
Wenbing Li <wenbingl@outlook.com>
2022-08-24 11:16:38 -0700

    build.sh issue on macos

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@
 set -e -x -u
 
 OSNAME=$(uname -s)
-if [[ "$OSNAME" == "Darwin" ]]; then
-  alias nproc="sysctl -n hw.logicalcpu"
+if [ -z ${CPU_NUMBER+x} ]; then
+  if [[ "$OSNAME" == "Darwin" ]]; then
+    CPU_NUMBER=$(sysctl -n hw.logicalcpu)
+  else
+    CPU_NUMBER=$(nproc)
+  fi
 fi
 
 BUILD_FLAVOR=RelWithDebInfo
 target_dir=out/$OSNAME/$BUILD_FLAVOR
 mkdir -p "$target_dir" && cd "$target_dir"
-if [ -z ${CPU_NUMBER+x} ]; then CPU_NUMBER=$(nproc); fi
 # it looks the parallel build on CI pipeline machine causes crashes.
 cmake "$@" ../../.. && cmake --build . --config $BUILD_FLAVOR  --parallel "${CPU_NUMBER}"
