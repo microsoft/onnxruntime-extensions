b3bb31f9da9d2bf88e7172c6861297df1c7acf8e
guschmue <guschmue@microsoft.com>
2022-09-16 11:23:29 -0700

    make spm build for static ort-web build

--- cmake/externals/sentencepieceproject.cmake
+++ cmake/externals/sentencepieceproject.cmake
@@ -8,14 +8,32 @@ FetchContent_GetProperties(spm)
 
 if(NOT spm_POPULATED)
   FetchContent_Populate(spm)
+  # need to patch sentencepiece to use protobuf provided by ort. This is ifdef onnxruntime_BUILD_WEBASSEMBLY
+  # for now but there is no real reason to not use it for all builds.
+  # 'git apply' should be in FetchContent_Declare() but that creates issues in sucessive builds so for now it is using execute_process().
+  if(onnxruntime_BUILD_WEBASSEMBLY)
+    set(SPM_PATCH "${PROJECT_SOURCE_DIR}/cmake/externals/sentencepieceproject.patch")
+    message("-- sentencepiece: patching with ${SPM_PATCH}")
+    execute_process(COMMAND git apply --ignore-space-change --ignore-whitespace ${SPM_PATCH} WORKING_DIRECTORY ${spm_SOURCE_DIR})
+  endif()
   add_subdirectory(${spm_SOURCE_DIR} ${spm_BINARY_DIR} EXCLUDE_FROM_ALL)
   set_target_properties(sentencepiece-static PROPERTIES
     FOLDER externals/google/sentencepiece)
 endif()
 
-set(spm_INCLUDE_DIRS
-    ${spm_SOURCE_DIR}/third_party/protobuf-lite
+if (onnxruntime_BUILD_WEBASSEMBLY)
+  set(SPM_USE_BUILTIN_PROTOBUF OFF)
+  set(spm_INCLUDE_DIRS
+    ${REPO_ROOT}/cmake/external/protobuf/src
     ${spm_SOURCE_DIR}/src/builtin_pb
     ${spm_SOURCE_DIR}/third_party
     ${spm_SOURCE_DIR}/src
-    )
+)
+else()
+  set(spm_INCLUDE_DIRS
+      ${spm_SOURCE_DIR}/third_party/protobuf-lite
+      ${spm_SOURCE_DIR}/src/builtin_pb
+      ${spm_SOURCE_DIR}/third_party
+      ${spm_SOURCE_DIR}/src
+      )
+endif()
