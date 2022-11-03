97f08bdde90c6741acb6aff42645a7a7dd6da097
Wenbing Li <wenbingl@outlook.com>
2022-11-03 15:28:41 -0700

    better name

--- java/src/test/android/app/src/androidTest/java/ai/onnxruntime/extensions/apptest/BertTokenizerTest.java
+++ java/src/test/android/app/src/androidTest/java/ai/onnxruntime/extensions/apptest/BertTokenizerTest.java
@@ -27,7 +27,7 @@ import ai.onnxruntime.extensions.OrtxPackage;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class BertTokenizerTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
