37bfbfe207e4db94650be4f5e8dbd1482331669e
Scott McKay <skottmckay@gmail.com>
2022-11-28 15:03:23 +1000

    Some progress in build setup

--- cmake/externals/libjpeg-turbo.cmake
+++ cmake/externals/libjpeg-turbo.cmake
@@ -1,10 +1,24 @@
-FetchContent_Declare(
-  libjpeg-turbo
-  GIT_REPOSITORY https://github.com/libjpeg-turbo/libjpeg-turbo.git
-  GIT_TAG        2.1.4
-  # GIT_SHALLOW    TRUE
+
+# set(ENABLE_SHARED OFF CACHE INTERNAL "")
+
+# FetchContent_Declare(
+#   libjpeg-turbo
+#   GIT_REPOSITORY https://github.com/libjpeg-turbo/libjpeg-turbo.git
+#   GIT_TAG        2.1.4
+#   # GIT_SHALLOW    TRUE
+#   PATCH_COMMAND git checkout . && git apply --whitespace=fix --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/cmake/externals/libjpeg-turbo.patch
+# )
+
+# FetchContent_MakeAvailable(libjpeg-turbo)
+
+ExternalProject_Add(libjpeg-turbo
+    GIT_REPOSITORY  https://github.com/libjpeg-turbo/libjpeg-turbo.git
+    GIT_TAG         2.1.4
+    GIT_SHALLOW     TRUE
+    PREFIX          _deps/libjpeg-turbo
+    INSTALL_COMMAND cmake -E echo "Skipping install step for dependency libjpeg-turbo"
+    INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}
 )
 
-FetchContent_MakeAvailable(libjpeg-turbo)
-
-# unset(EXECUTABLE_OUTPUT_PATH CACHE)
+set (libjpeg-turbo_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/libjpeg-turbo/src)
+# D:\src\github\ort-extensions\build\Windows\Debug\_deps\libjpeg-turbo\src\libjpeg-turbo
---
+++ cmake/externals/zlib.cmake
@@ -0,0 +1,32 @@
+# ExternalProject_Add(zlib
+#     PREFIX zlib
+#     GIT_REPOSITORY https://github.com/madler/zlib.git
+#     GIT_TAG v1.2.13
+#     GIT_SHALLOW TRUE
+# )
+
+# message(STATUS "src:${zlib_SOURCE_DIR} ")
+
+FetchContent_Declare(
+    zlib
+    GIT_REPOSITORY  "https://github.com/madler/zlib.git"
+    GIT_TAG         v1.2.13
+    GIT_SHALLOW     TRUE
+)
+
+FetchContent_GetProperties(zlib)
+if(NOT zlib_POPULATED)
+    FetchContent_Populate(zlib)
+
+    add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
+
+    target_include_directories(zlib PUBLIC
+        $<BUILD_INTERFACE:${zlib_BINARY_DIR}>
+        $<INSTALL_INTERFACE:include>
+    )
+
+    target_include_directories(zlibstatic PUBLIC
+        $<BUILD_INTERFACE:${zlib_BINARY_DIR}>
+        $<INSTALL_INTERFACE:include>
+    )
+endif()
