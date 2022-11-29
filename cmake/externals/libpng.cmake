0192a71ef3a96645d9ddb51517587294c5db9220
Scott McKay <skottmckay@gmail.com>
2022-11-29 12:07:30 +1000

    Make Linux build work

--- cmake/externals/libpng.cmake
+++ cmake/externals/libpng.cmake
@@ -12,9 +12,12 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(libpng)
 
-# assumes static zlib is already available. can probably clean this up if we switch to using vcpkg for zlib
-add_dependencies(png_static zlibstatic)
-target_link_libraries(png_static zlibstatic)
+if (MSVC)
+  # assume we're building zlib and need to manually specify the dependency.
+  add_dependencies(png_static ${ZLIB_LIBRARIES})
+endif()
+
+target_link_libraries(png_static ${ZLIB_LIBRARIES})
 target_include_directories(png_static PRIVATE ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
 
 set(libpng_INCLUDE_DIRS ${libpng_SOURCE_DIR} ${libpng_BINARY_DIR})
--- cmake/externals/zlib.cmake
+++ cmake/externals/zlib.cmake
@@ -1,38 +1,17 @@
-# ExternalProject_Add(zlib
-#     PREFIX zlib
-#     GIT_REPOSITORY https://github.com/madler/zlib.git
-#     GIT_TAG v1.2.13
-#     GIT_SHALLOW TRUE
-# )
+if (NOT MSVC)
+    # Assume zlib is found using find_package. TODO: Could fallback to FetchContent if find_package fails.
+    find_package(ZLIB REQUIRED)
+    set(zlib_INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS})
+    set(zlib_LIB_NAME ${ZLIB_LIBRARIES})
+else()
+    FetchContent_Declare(
+        zlib
+        GIT_REPOSITORY  "https://github.com/madler/zlib.git"
+        GIT_TAG         v1.2.13
+        GIT_SHALLOW     TRUE
+    )
 
-# message(STATUS "src:${zlib_SOURCE_DIR} ")
-
-
-FetchContent_Declare(
-    zlib
-    GIT_REPOSITORY  "https://github.com/madler/zlib.git"
-    GIT_TAG         v1.2.13
-    GIT_SHALLOW     TRUE
-)
-
-# FetchContent_GetProperties(zlib)
-# if(NOT zlib_POPULATED)
-#     FetchContent_Populate(zlib)
-
-#     add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
-
-#     target_include_directories(zlib PUBLIC
-#         $<BUILD_INTERFACE:${zlib_BINARY_DIR}>
-#         $<INSTALL_INTERFACE:include>
-#     )
-
-#     target_include_directories(zlibstatic PUBLIC
-#         $<BUILD_INTERFACE:${zlib_BINARY_DIR}>
-#         $<INSTALL_INTERFACE:include>
-#     )
-# endif()
-
-FetchContent_MakeAvailable(zlib)
-
-set(zlib_INCLUDE_DIRS ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
-set(zlib_LIB_NAME zlibstatic)
+    FetchContent_MakeAvailable(zlib)
+    set(zlib_INCLUDE_DIRS ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
+    set(ZLIB_LIBRARIES zlibstatic)
+endif()
