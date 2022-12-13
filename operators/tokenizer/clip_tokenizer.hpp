4ff821805a811a4c4e04e17485191d813f7e4834
Sayan Shaw <52221015+sayanshaw24@users.noreply.github.com>
2022-12-13 15:52:47 -0800

    Initial CLIP tokenizer implementation (#323)
    * Initial CLIP tokenizer implementation
    
    * Moved common code from CLIP and GPT2 tokenizers into separate file
    
    * add the new file into cmake file list.
    
    * Fix ustring reference issue
    
    * merge changes from main branch
    
    * more merge actions
    
    * Minor changes
    
    Co-authored-by: Sayan Shaw <sayanshaw@microsoft.com>
    Co-authored-by: Wenbing Li <10278425+wenbingl@users.noreply.github.com>
    Co-authored-by: Wenbing Li <wenbingl@outlook.com>

---
+++ operators/tokenizer/clip_tokenizer.hpp
@@ -0,0 +1,27 @@
+#include <list>
+#include "ocos.h"
+#include "ustring.h"
+#include "string_utils.h"
+
+class VocabData;
+
+struct KernelClipBpeTokenizer : BaseKernel {
+  KernelClipBpeTokenizer(const OrtApi& api, const OrtKernelInfo* info);
+  void Compute(OrtKernelContext* context);
+
+ private:
+  std::vector<int64_t> Tokenize(ustring& input, int64_t max_length);
+
+  int64_t padding_length_;
+  std::list<int> byte_list_;
+  std::shared_ptr<VocabData> bbpe_tokenizer_;
+};
+
+struct CustomOpClipBpeTokenizer : OrtW::CustomOpBase<CustomOpClipBpeTokenizer, KernelClipBpeTokenizer> {
+  void* CreateKernel(const OrtApi& api, const OrtKernelInfo* info) const;
+  const char* GetName() const;
+  size_t GetInputTypeCount() const;
+  ONNXTensorElementDataType GetInputType(size_t index) const;
+  size_t GetOutputTypeCount() const;
+  ONNXTensorElementDataType GetOutputType(size_t index) const;
+};
