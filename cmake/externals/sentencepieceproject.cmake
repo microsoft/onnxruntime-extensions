c40fbe7e2d68d120d3f05bed8723a6a5befca7f8
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2022-12-16 18:11:46 -0800

    Patch sentencepiece to enable iOS build.

--- cmake/externals/sentencepieceproject.cmake
+++ cmake/externals/sentencepieceproject.cmake
@@ -8,14 +8,16 @@ FetchContent_GetProperties(spm)
 
 if(NOT spm_POPULATED)
   FetchContent_Populate(spm)
-  # need to patch sentencepiece to use protobuf provided by ort. This is ifdef onnxruntime_BUILD_WEBASSEMBLY
-  # for now but there is no real reason to not use it for all builds.
   # 'git apply' should be in FetchContent_Declare() but that creates issues in sucessive builds so for now it is using execute_process().
-  if(onnxruntime_BUILD_WEBASSEMBLY)
-    set(SPM_PATCH "${PROJECT_SOURCE_DIR}/cmake/externals/sentencepieceproject.patch")
+  foreach(SPM_PATCH IN ITEMS
+          # use protobuf provided by ORT
+          "${PROJECT_SOURCE_DIR}/cmake/externals/sentencepieceproject.protobuf.patch"
+          # fix for iOS build
+          "${PROJECT_SOURCE_DIR}/cmake/externals/sentencepieceproject.ios.patch"
+          )
     message("-- sentencepiece: patching with ${SPM_PATCH}")
     execute_process(COMMAND git apply --ignore-space-change --ignore-whitespace ${SPM_PATCH} WORKING_DIRECTORY ${spm_SOURCE_DIR})
-  endif()
+  endforeach()
   add_subdirectory(${spm_SOURCE_DIR} ${spm_BINARY_DIR} EXCLUDE_FROM_ALL)
   set_target_properties(sentencepiece-static PROPERTIES
     FOLDER externals/google/sentencepiece)
