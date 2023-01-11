d7f78612e7d1b8262f6f7e4d53c45af9e415a546
Sayan Shaw <sayanshaw@microsoft.com>
2023-01-11 11:27:00 -0800

    Added arg parsing, error check, and modularized methods

--- tools/gen_customop_template.py
+++ tools/gen_customop_template.py
@@ -4,64 +4,95 @@ Generates the C++ template code for a new custom operator.
 Usage: $ python tools\gen_customop_template.py onnx_model_filepath
 '''
 
+import argparse
 import onnx
+import pathlib
 import sys
 
 # Load and check ONNX model
-model = onnx.load(sys.argv[1])
-onnx.checker.check_model(model)
+def load_onnx_model(model_path):
+    model = onnx.load(model_path)
+    onnx.checker.check_model(model)
+    return model
 
 # Get input and output type count from the ONNX model using protobuf information
-input_count = 0
-output_count = 0
+def get_io_count(model):
+    input_count = 0
+    output_count = 0
+    custom_op_node_exists = False
 
-# Note: if there is no custom op node in the graph, GetInputTypeCount() and
-# GetOutputTypeCount() will return 0.
-for node in model.graph.node:
-    # Find CustomOp node using domain
-    if node.domain == "ai.onnx.contrib" or node.domain == "com.microsoft.extensions":
-        input_count = len(node.input)
-        output_count = len(node.output)
+    for node in model.graph.node:
+        # Find CustomOp node using domain
+        if node.domain == "ai.onnx.contrib" or node.domain == "com.microsoft.extensions":
+            custom_op_node_exists = True
+            input_count = len(node.input)
+            output_count = len(node.output)
 
-# Initial CustomOp code to be populated in C++ header file for CustomOp template
-customop_template = r'''
-#include <stdio.h>
-#include "ocos.h"
-#include "string_utils.h"
-#include "ustring.h"
+    if not custom_op_node_exists:
+        sys.exit("Error: No custom op node present in provided model")
 
-void* {custom_op}::CreateKernel(const OrtApi& api, const OrtKernelInfo* info) const {{
-  return CreateKernelImpl(api, info);
-}};
+    return input_count, output_count
 
-const char* {custom_op}::GetName() const {{ return "{op_name}"; }};
+# Add initial CustomOp code to C++ header file for CustomOp template
+def create_hpp(op, op_name, input_type_count, output_type_count):
+    customop_template = r'''
+    #include <stdio.h>
+    #include "ocos.h"
+    #include "string_utils.h"
+    #include "ustring.h"
 
-size_t {custom_op}::GetInputTypeCount() const {{
-  return {input_type_count};
-}};
+    void* {custom_op}::CreateKernel(const OrtApi& api, const OrtKernelInfo* info) const {{
+      return CreateKernelImpl(api, info);
+    }};
 
-// Note: the following method is not complete and contains a temporary default return value.
-// Change return value to appropriate data type based on ONNXTensorElementDataType
-// mapping of TensorProto.DataType value
-ONNXTensorElementDataType {custom_op}::GetInputType(size_t /*index*/) const {{
-  return ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING;
-}};
+    const char* {custom_op}::GetName() const {{ return "{op_name}"; }};
 
-size_t {custom_op}::GetOutputTypeCount() const {{
-  return {output_type_count};
-}};
+    size_t {custom_op}::GetInputTypeCount() const {{
+      return {input_type_count};
+    }};
 
-// Note: the following method is not complete and contains a temporary default return value.
-// Change return value to appropriate data type based on ONNXTensorElementDataType
-// mapping of TensorProto.DataType value
-ONNXTensorElementDataType {custom_op}::GetOutputType(size_t /*index*/) const {{
-  return ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING;
-}};
-'''
+    // Note: the following method is not complete and contains a temporary default return value.
+    // Change return value to appropriate data type based on ONNXTensorElementDataType
+    // mapping of TensorProto.DataType value
+    ONNXTensorElementDataType {custom_op}::GetInputType(size_t /*index*/) const {{
+      return ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING;
+    }};
 
-cpp_file = customop_template.format(custom_op = "CustomOp", op_name = "CustomOpName", input_type_count = input_count, output_type_count = output_count)
+    size_t {custom_op}::GetOutputTypeCount() const {{
+      return {output_type_count};
+    }};
 
-# Write code to C++ temmplate file
-new_op = "tools/custom_op_template.hpp"
-with open(new_op,'wt') as file:
-    file.write(cpp_file)
+    // Note: the following method is not complete and contains a temporary default return value.
+    // Change return value to appropriate data type based on ONNXTensorElementDataType
+    // mapping of TensorProto.DataType value
+    ONNXTensorElementDataType {custom_op}::GetOutputType(size_t /*index*/) const {{
+      return ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING;
+    }};
+    '''
+
+    hpp_file = customop_template.format(custom_op = op, op_name = op_name, input_type_count = input_type_count, output_type_count = output_type_count)
+
+    # Write code to C++ temmplate file
+    new_op = "tools/custom_op_template.hpp"
+    with open(new_op,'wt') as file:
+        file.write(hpp_file)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate C++ template code for a new custom operator.",
+    )
+
+    parser.add_argument(
+        "onnx_model_filepath",
+        type=pathlib.Path,
+        help="Path to ONNX model with CustomOp node.",
+    )
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    model = load_onnx_model(args.onnx_model_filepath)
+    input_count, output_count = get_io_count(model)
+    create_hpp(op = "CustomOp", op_name = "CustomOpName", input_type_count = input_count, output_type_count = output_count)
