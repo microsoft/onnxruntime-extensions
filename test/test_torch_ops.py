34bc844e301a4218ead1002320770e6f68c6b8d0
Wenbing Li <wenbingl@outlook.com>
2023-02-06 10:15:27 -0800

    torch version detection

--- test/test_torch_ops.py
+++ test/test_torch_ops.py
@@ -21,7 +21,7 @@ def my_inverse(g, self):
     return g.op("ai.onnx.contrib::Inverse", self)
 
 
-if LooseVersion(torch.__version__) > LooseVersion("1.12.0"):
+if LooseVersion(torch.__version__) >= LooseVersion("1.13"):
     register_custom_op_symbolic('::linalg_inv', my_inverse, 1)
 else:
     register_custom_op_symbolic('::inverse', my_inverse, 1)
