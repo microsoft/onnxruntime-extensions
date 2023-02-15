bf2f159629d022cd0c11369abb191f82781542f0
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2023-02-14 19:49:02 -0800

    Update cmake/externals/sentencepieceproject.cmake
    Co-authored-by: Edward Chen <18449977+edgchen1@users.noreply.github.com>

--- cmake/externals/sentencepieceproject.cmake
+++ cmake/externals/sentencepieceproject.cmake
@@ -42,7 +42,7 @@ endif()
 
 # To avoid create a complicated logic to build protoc, especailly for mobile platforms, we use the pre-generated pb files
 # Uses the following command line in _deps/spm-src folder to generate the PB patch file if protobuf version is updated
-# git diff -- src/builtin_pb/* | out-file -Encoding utf8 <REPO-ROOT>\cmake\externals\sentencepieceproject.pb.patch
+# git diff -- src/builtin_pb/* | out-file -Encoding utf8 <REPO-ROOT>\cmake\externals\sentencepieceproject_pb.patch
 # PB files was seperated as another patch file to avoid the patch file too large to be reviewed.
 set(spm_patches
   "${PROJECT_SOURCE_DIR}/cmake/externals/sentencepieceproject_cmake.patch"
