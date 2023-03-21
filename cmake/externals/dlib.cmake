39a32f68c7163a24581372e9be80a05478302504
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2023-03-21 15:39:23 -0700

    Update dlib.cmake

--- cmake/externals/dlib.cmake
+++ cmake/externals/dlib.cmake
@@ -1,5 +1,7 @@
 FetchContent_Declare(dlib
     GIT_REPOSITORY https://github.com/davisking/dlib.git
+    # there is non an official tag which supports STFT,
+    # choose a relatively stable commit id for that.
     GIT_TAG        a12824d42584e292ecb3bad05c4b32c2015a7b89
 )
 
