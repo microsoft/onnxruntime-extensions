20e3082e47ac7d1aa1f88d2385656a7a84626135
Wenbing Li <wenli@microsoft.com>
2023-04-14 13:26:40 -0700

    add merged models in whisper model

--- onnxruntime_extensions/pnp/__init__.py
+++ onnxruntime_extensions/pnp/__init__.py
@@ -5,6 +5,8 @@ except ImportError as e:
     print("No torch installation found, which is required by the pre&post scripting!")
     raise e
 
+from ._onnx_ops import ONNXElementContainer, ox, make_model_ex
+
 from ._base import ProcessingTracedModule, ProcessingScriptModule, CustomFunction
 from ._torchext import *  # noqa
 from ._unifier import export
