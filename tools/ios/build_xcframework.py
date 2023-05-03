c666182bf310ae3c539b83be00beb949651054fa
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2023-05-02 20:24:17 -0700

    refine build_xcframework.py

--- tools/ios/build_xcframework.py
+++ tools/ios/build_xcframework.py
@@ -54,7 +54,7 @@ def build_framework_for_platform_and_arch(
     opencv_dir: Path,
     ios_deployment_target: str,
     cmake_extra_defines: List[str],
-) -> Path:
+):
     build_dir.mkdir(parents=True, exist_ok=True)
 
     # generate build files
@@ -88,13 +88,11 @@ def build_framework_for_platform_and_arch(
     # build
     _run([_cmake, f"--build", f"{build_dir}", f"--config={config}", "--parallel"])
 
-    return build_dir / "static_framework/onnxruntime_extensions.framework"
 
-
-def build_artifact_by_target(
+def build_xcframework(
     output_dir: Path,
     platform_archs: Dict[str, List[str]],
-    build_target: str,
+    mode: str,
     config: str,
     opencv_dir: Path,
     ios_deployment_target: str,
@@ -106,23 +104,19 @@ def build_artifact_by_target(
 
     assert len(platform_archs) > 0, "no platforms specified"
 
-    # the public headers and framework_info.json should be the same across platform/arch builds
-    # select them from one of the platform/arch build directories to copy to the output directory
-    headers_dir = None
-    framework_info_file = None
-
-    platform_fat_framework_dirs = []
     for platform, archs in platform_archs.items():
-        platform_fat_framework_dir = intermediate_build_dir / f"{platform}/onnxruntime_extensions.framework"
-        platform_fat_framework_dirs.append(platform_fat_framework_dir)
-    
-    if build_target in ["lib", "xcframework"]:
-        for idx, (platform, archs) in enumerate(platform_archs.items()):
-            assert len(archs) > 0, f"no arch specified for platform {platform}"
-            arch_framework_dirs = []
+        assert len(archs) > 0, f"no arch specified for platform {platform}"
+
+    def platform_arch_framework_build_dir(platform, arch):
+        return intermediate_build_dir / f"{platform}/{arch}"
+
+    build_platform_arch_frameworks = mode in ["build_platform_arch_frameworks_only", "build_xcframework"]
+
+    if build_platform_arch_frameworks:
+        for platform, archs in platform_archs.items():
             for arch in archs:
-                arch_framework_dir = build_framework_for_platform_and_arch(
-                    intermediate_build_dir / f"{platform}/{arch}/{config}",
+                build_framework_for_platform_and_arch(
+                    platform_arch_framework_build_dir(platform, arch),
                     platform,
                     arch,
                     config,
@@ -131,19 +125,42 @@ def build_artifact_by_target(
                     cmake_extra_defines,
                 )
 
-                arch_framework_dirs.append(arch_framework_dir)
+    pack_xcframework = mode in ["pack_xcframework_only", "build_xcframework"]
 
-                if headers_dir is None:
-                    headers_dir = arch_framework_dir / "Headers"
-                    framework_info_file = arch_framework_dir.parents[1] / "framework_info.json"
+    if pack_xcframework:
+        # the public headers and framework_info.json should be the same across platform/arch builds
+        # select them from one of the platform/arch build directories to copy to the output directory
+        headers_dir = None
+        framework_info_file = None
+
+        # create per-platform fat framework from platform/arch frameworks
+        platform_fat_framework_dirs = []
+        for platform, archs in platform_archs.items():
+            arch_framework_dirs = [
+                platform_arch_framework_build_dir(platform, arch) / "static_framework/onnxruntime_extensions.framework"
+                for arch in archs
+            ]
+
+            if not build_platform_arch_frameworks:
+                # if they weren't just built, check that the expected platform/arch framework directories are present
+                for arch_framework_dir in arch_framework_dirs:
+                    assert (
+                        arch_framework_dir.is_dir()
+                    ), f"platform/arch framework directory not found: {arch_framework_dir}"
+
+            first_arch_framework_dir = arch_framework_dirs[0]
+
+            if headers_dir is None:
+                headers_dir = first_arch_framework_dir / "Headers"
+                framework_info_file = first_arch_framework_dir.parents[1] / "framework_info.json"
 
             platform_fat_framework_dir = intermediate_build_dir / f"{platform}/onnxruntime_extensions.framework"
             _rmtree_if_existing(platform_fat_framework_dir)
             platform_fat_framework_dir.mkdir()
 
-            # copy over files from arch-specific framework
+            # copy over files from arch-specific framework to fat framework
             for framework_file_relative_path in [Path("Headers"), Path("Info.plist")]:
-                src = arch_framework_dirs[0] / framework_file_relative_path
+                src = first_arch_framework_dir / framework_file_relative_path
                 dst = platform_fat_framework_dir / framework_file_relative_path
                 if src.is_dir():
                     shutil.copytree(src, dst)
@@ -154,30 +171,24 @@ def build_artifact_by_target(
             arch_libs = [str(framework_dir / "onnxruntime_extensions") for framework_dir in arch_framework_dirs]
             _run([_lipo, "-create", "-output", str(platform_fat_framework_dir / "onnxruntime_extensions")] + arch_libs)
 
-            assert platform_fat_framework_dirs[idx] == platform_fat_framework_dir
-    
-    if headers_dir:
+            platform_fat_framework_dirs.append(platform_fat_framework_dir)
+
+        # create xcframework
+        xcframework_dir = output_dir / "onnxruntime_extensions.xcframework"
+        _rmtree_if_existing(xcframework_dir)
+
+        create_xcframework_args = [_xcrun, "xcodebuild", "-create-xcframework", "-output", str(xcframework_dir)]
+        for platform_fat_framework_dir in platform_fat_framework_dirs:
+            create_xcframework_args += ["-framework", str(platform_fat_framework_dir)]
+        _run(create_xcframework_args)
+
         # copy public headers
         output_headers_dir = output_dir / "Headers"
         _rmtree_if_existing(output_headers_dir)
         shutil.copytree(headers_dir, output_headers_dir)
 
-    if framework_info_file:
         # copy framework_info.json
         shutil.copyfile(framework_info_file, output_dir / "framework_info.json")
-
-    # if build lib only, we're done
-    if build_target == "lib":
-        return
-
-    # create xcframework
-    xcframework_dir = output_dir / "onnxruntime_extensions.xcframework"
-    _rmtree_if_existing(xcframework_dir)
-
-    create_xcframework_args = [_xcrun, "xcodebuild", "-create-xcframework", "-output", str(xcframework_dir)]
-    for platform_fat_framework_dir in platform_fat_framework_dirs:
-        create_xcframework_args += ["-framework", str(platform_fat_framework_dir)]
-    _run(create_xcframework_args)
 
 
 def parse_args():
@@ -191,20 +202,32 @@ def parse_args():
         required=True,
         help="Path to output directory.",
     )
-    # Same as how android works
-    # This one is used in the ci pipeline for accelerating the build process,
-    # We have 4 archs to be build. It's in sequence by default, but we can build them in parallel.
-    # The parallel build works as:
-    #   1. build the lib files for each arch in different ci jobs
-    #   2. download all the lib files in tasks
-    #   2. pack the xcframework package in ci task
+    # This option is used in CI pipelines to accelerate the build process,
+    # We have multiple platform/archs to build for. We can build them in parallel.
+    # The parallel build works like this:
+    #   1. build the platform/arch frameworks in different jobs, in parallel
+    #   2. download the platform/arch framework files from the previous jobs and pack them into the xcframework
     parser.add_argument(
-        "--build_target",
-        default="xcframework",
-        choices=["lib", "xcframework", "pack_xcframework"],
-        help="Build target. 'xcframework' builds the whole package. 'lib' builds the libraries only. \
-            'pack_xcframework' only does pack xcframework from existing lib files.",
+        "--mode",
+        default="build_xcframework",
+        choices=["build_xcframework", "build_platform_arch_frameworks_only", "pack_xcframework_only"],
+        help="Build mode. "
+        "'build_xcframework' builds the whole package. "
+        "'build_platform_arch_frameworks_only' builds the platform/arch frameworks only. "
+        "'pack_xcframework_only' packs the xcframework from existing lib files only. "
+        "Note: 'pack_xcframework_only' assumes previous invocation(s) with mode 'build_platform_arch_frameworks_only'.",
     )
+    parser.add_argument(
+        "--platform-arch",
+        nargs=2,
+        action="append",
+        metavar=("PLATFORM", "ARCH"),
+        dest="platform_archs",
+        help="Specify a platform/arch pair to build. Repeat to specify multiple pairs. "
+        "If no pairs are specified, all supported pairs will be built.",
+    )
+
+    # platform/arch framework build-related options
     parser.add_argument(
         "--config",
         choices=["Debug", "Release", "RelWithDebInfo", "MinSizeRel"],
@@ -215,16 +238,6 @@ def parse_args():
         "--ios-deployment-target",
         default="11.0",
         help="iOS deployment target.",
-    )
-    
-    parser.add_argument(
-        "--platform-arch",
-        nargs=2,
-        action="append",
-        metavar=("PLATFORM", "ARCH"),
-        dest="platform_archs",
-        help="Specify a platform/arch pair to build. Repeat to specify multiple pairs. "
-        "If no pairs are specified, all supported pairs will be built.",
     )
     parser.add_argument(
         "--cmake-extra-defines",
@@ -269,10 +282,10 @@ def parse_args():
 def main():
     args = parse_args()
 
-    build_artifact_by_target(
+    build_xcframework(
         output_dir=args.output_dir,
         platform_archs=args.platform_archs,
-        build_target=args.build_target,
+        mode=args.mode,
         config=args.config,
         opencv_dir=_repo_dir / "cmake/externals/opencv",
         ios_deployment_target=args.ios_deployment_target,
