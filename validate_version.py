93343da61854650e30ddd61ab77b3efb02e883e3
Scott McKay <skottmckay@gmail.com>
2023-05-06 14:08:14 +1000

    cleanup bad merge

--- validate_version.py
+++
@@ -1,9 +0,0 @@
-import semver
-import sys
-
-input = sys.argv[1]
-ver = semver.Version.parse(input)
-if ver.prerelease:
-    prefix = ver.prerelease.split('.')[0]
-    if not prefix in ('alpha', 'beta', 'rc'):
-        raise ValueError(f"Invalid pre-release. (alpha|beta|rc) accepted.")
