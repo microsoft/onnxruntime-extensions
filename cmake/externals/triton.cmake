f8d245d893ac808d0ef42a95473cb2524034ee7d
RandySheriffH <48490400+RandySheriffH@users.noreply.github.com>
2023-07-02 22:17:43 -0700

    Fix pipelines (#479)
    * 1
    
    * 2
    
    * 3
    
    * 4
    
    * 5
    
    * 6
    
    * 7
    
    * 8
    
    * 9
    
    * 10
    
    * 11
    
    * 12
    
    * 13
    
    * 14
    
    * 15
    
    * 16
    
    * 17
    
    * 18
    
    * 19
    
    * 20
    
    * 21
    
    * 22
    
    * 23
    
    * 24
    
    * 25
    
    * 26
    
    * 27
    
    * 28
    
    * 29
    
    * 30
    
    * 31
    
    * 32
    
    * 33
    
    * 34
    
    * 35
    
    * 36
    
    * 37
    
    * 38
    
    * 39
    
    * 40
    
    * 41
    
    * 42
    
    * 43
    
    * 44
    
    * 45
    
    * 46
    
    * 47
    
    * 47
    
    * 48
    
    * 49
    
    * 50
    
    * 51
    
    * 52
    
    * 53
    
    * 54
    
    * 55
    
    * 56
    
    * 57
    
    * 58
    
    * 59
    
    * 60
    
    * 61
    
    * 62
    
    * 62
    
    * 63
    
    * 64
    
    * 65
    
    * 66
    
    * 67
    
    * 68
    
    * 69
    
    * 70
    
    * 71
    
    * 72
    
    * 73
    
    * 74
    
    * 75
    
    * 76
    
    * 77
    
    * 78
    
    * 79:
    
    * 80:
    
    ---------
    
    Co-authored-by: Randy Shuai <rashuai@microsoft.com>

--- cmake/externals/triton.cmake
+++ cmake/externals/triton.cmake
@@ -3,34 +3,44 @@ include(ExternalProject)
 if (WIN32)
 
   if (ocos_target_platform STREQUAL "AMD64")
-    set(vcpkg_target_platform "x64")
+    set(vcpkg_target_platform "x86")
   else()
     set(vcpkg_target_platform ${ocos_target_platform})
   endif()
 
-  function(get_vcpkg)
-    ExternalProject_Add(vcpkg
-      GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
-      GIT_TAG 2022.11.14
-      PREFIX vcpkg
-      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src
-      BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-build
-      CONFIGURE_COMMAND ""
-      INSTALL_COMMAND ""
-      UPDATE_COMMAND ""
-      BUILD_COMMAND "<SOURCE_DIR>/bootstrap-vcpkg.bat")
+  ExternalProject_Add(vcpkg
+    GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
+    GIT_TAG 2023.06.20
+    PREFIX vcpkg
+    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src
+    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-build
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+    BUILD_COMMAND "<SOURCE_DIR>/bootstrap-vcpkg.bat")
 
-    ExternalProject_Get_Property(vcpkg SOURCE_DIR)
-    set(VCPKG_SRC ${SOURCE_DIR} PARENT_SCOPE)
-    set(VCPKG_DEPENDENCIES "vcpkg" PARENT_SCOPE)
-  endfunction()
+  set(VCPKG_SRC ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src)
+  set(ENV{VCPKG_ROOT} ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src)
+
+  message(STATUS "VCPKG_SRC: " ${VCPKG_SRC})
+  message(STATUS "VCPKG_ROOT: " $ENV{VCPKG_ROOT})
+
+  add_custom_command(
+    COMMAND ${VCPKG_SRC}/vcpkg integrate install
+    COMMAND ${CMAKE_COMMAND} -E touch vcpkg_integrate.stamp
+    OUTPUT vcpkg_integrate.stamp
+    DEPENDS vcpkg
+  )
+
+  add_custom_target(vcpkg_integrate ALL DEPENDS vcpkg_integrate.stamp)
+  set(VCPKG_DEPENDENCIES "vcpkg_integrate")
 
   function(vcpkg_install PACKAGE_NAME)
     add_custom_command(
       OUTPUT ${VCPKG_SRC}/packages/${PACKAGE_NAME}_${vcpkg_target_platform}-windows-static/BUILD_INFO
-      COMMAND ${VCPKG_SRC}/vcpkg install ${PACKAGE_NAME}:${vcpkg_target_platform}-windows-static
+      COMMAND ${VCPKG_SRC}/vcpkg install ${PACKAGE_NAME}:${vcpkg_target_platform}-windows-static --vcpkg-root=${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src
       WORKING_DIRECTORY ${VCPKG_SRC}
-      DEPENDS vcpkg)
+      DEPENDS vcpkg_integrate)
 
     add_custom_target(get${PACKAGE_NAME}
       ALL
@@ -40,7 +50,6 @@ if (WIN32)
     set(VCPKG_DEPENDENCIES ${VCPKG_DEPENDENCIES} PARENT_SCOPE)
   endfunction()
 
-  get_vcpkg()
   vcpkg_install(openssl)
   vcpkg_install(openssl-windows)
   vcpkg_install(rapidjson)
@@ -72,6 +81,17 @@ if (WIN32)
 
 else()
 
+  if(DEFINED ENV{IS_DOCKER_BUILD})
+    message(STATUS "IS_DOCKER_BUILD set")
+    ExternalProject_Add(curl7
+                        PREFIX curl7
+                        GIT_REPOSITORY "https://github.com/curl/curl.git"
+                        GIT_TAG "curl-7_86_0"
+                        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/curl7-src
+                        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/curl7-build
+                        CMAKE_ARGS -DBUILD_TESTING=OFF -DBUILD_CURL_EXE=OFF -DBUILD_SHARED_LIBS=OFF -DCURL_STATICLIB=ON -DHTTP_ONLY=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
+  endif()
+
   ExternalProject_Add(triton
                       GIT_REPOSITORY https://github.com/triton-inference-server/client.git
                       GIT_TAG r23.05
@@ -81,6 +101,10 @@ else()
                       CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=binary -DTRITON_ENABLE_CC_HTTP=ON -DTRITON_ENABLE_ZLIB=OFF
                       INSTALL_COMMAND ""
                       UPDATE_COMMAND "")
+
+  if(DEFINED ENV{IS_DOCKER_BUILD})
+    add_dependencies(triton curl7)
+  endif()
 
 endif() #if (WIN32)
 
