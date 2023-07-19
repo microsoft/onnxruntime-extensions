ab82a2b0c5b07bb676d03300150d60aa74bae0cb
Randy Shuai <rashuai@microsoft.com>
2023-07-19 10:18:47 -0700

    16

--- install_deps.bat
+++ install_deps.bat
@@ -1,2 +1,7 @@
 @echo off
-if "%OCOS_ENABLE_AZURE%"=="1" (pip install cmake)
+if "%OCOS_ENABLE_AZURE%"=="1" (
+  pip install cmake
+  if not exist "C:\Program Files\Miniconda3\python3.exe" (
+    mklink "C:\Program Files\Miniconda3\python3.exe" "C:\Program Files\Miniconda3\python.exe"
+  )
+)
