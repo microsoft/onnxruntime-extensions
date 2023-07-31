3ac031e640486c583d97469b81f2de594cfa271f
Randy Shuai <rashuai@microsoft.com>
2023-07-31 15:48:08 -0700

    install ort

--- tools/test_cibuildwheel.bat
+++ tools/test_cibuildwheel.bat
@@ -1,6 +1,7 @@
 @echo off
 if "%OCOS_ENABLE_AZURE%"=="1" (
     pushd %1\test
+    python install onnxruntime
     python test_azure_ops.py
     popd
 )
--- tools/test_cibuildwheel.sh
+++ tools/test_cibuildwheel.sh
@@ -3,6 +3,7 @@
 if [[ "$OCOS_ENABLE_AZURE" == "1" ]]
 then
     pushd $1/test
+    python install onnxruntime
     python ./test_azure_ops.py
     popd
 fi
