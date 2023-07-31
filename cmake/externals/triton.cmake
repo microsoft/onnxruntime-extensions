ffda9f893644ca395495da4863973790d2f32b02
Scott McKay <Scott.McKay@microsoft.com>
2023-07-31 17:07:26 +1000

    CMake setup changes to try and build the Azure custom op code cleanly on Linux and Windows.
    Use PREFIX for ExternalProject_Add so everything is under one directory under _deps.
    - currently we have some things under the top level build dir and others in 'src' and 'build' directories for the project under _deps.
    
    Fix Windows build warnings and use 'static-md' vcpkg target so debug and release builds work (previously had warning about mixing dependencies on the debug and release CRT libraries)
    
    Remove packages and dependencies that do not seem to be required.
    
    Build RapidJSON on Linux using cmake so it doesn't need to be installed.
    
    Use URL and hash for triton client (usually required by security reviews)
    
    Fix build of tests when RE2 is disabled
    
    Fix some type mismatches in '--clean' command in build.py
    
    Open issues:
    - Should we use the system curl header/lib on Linux or the triton client builds?
      - seems like using the system one from the install of 'libcurl4-openssl-dev' would be better to ensure that the curl and openssl components were compatible
      - I couldn't find a way to get the triton client build to use those. it wants to download and build its own version of curl. not sure if that inconsistency between curl packages will be an issue.
    - Currently whether it uses libcurl or libcurl-d on Windows is hardcoded. Hopefully there's a way to get the correct library name from vcpkg based on the build config (Debug or not).

--- cmake/externals/triton.cmake
+++ cmake/externals/triton.cmake
@@ -1,5 +1,8 @@
 include(ExternalProject)
 
+set(triton_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/_deps/triton)
+set(triton_INSTALL_DIR ${triton_PREFIX}/install)
+
 if (WIN32)
   if (ocos_target_platform STREQUAL "AMD64")
     set(vcpkg_target_platform "x64")
@@ -7,25 +10,32 @@ if (WIN32)
     set(vcpkg_target_platform ${ocos_target_platform})
   endif()
 
-  ExternalProject_Add(vcpkg
-    GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
-    GIT_TAG 2023.06.20
-    PREFIX vcpkg
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-build
-    CONFIGURE_COMMAND ""
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
-    BUILD_COMMAND "<SOURCE_DIR>/bootstrap-vcpkg.bat")
+  set(vcpkg_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg)
 
-  set(VCPKG_SRC ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src)
-  set(ENV{VCPKG_ROOT} ${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src)
+  ExternalProject_Add(vcpkg
+                      GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
+                      GIT_TAG 2023.06.20
+                      PREFIX ${vcpkg_PREFIX}
+                      CONFIGURE_COMMAND ""
+                      INSTALL_COMMAND ""
+                      UPDATE_COMMAND ""
+                      BUILD_COMMAND "<SOURCE_DIR>/bootstrap-vcpkg.bat")
+
+  ExternalProject_Get_Property(vcpkg SOURCE_DIR BINARY_DIR)
+  set(VCPKG_SRC ${SOURCE_DIR})
+  message(status "vcpkg source dir: " ${VCPKG_SRC})
+
+  # set the environment variable so that the vcpkg.cmake file can find the vcpkg root directory
+  set(ENV{VCPKG_ROOT} ${VCPKG_SRC})
 
   message(STATUS "VCPKG_SRC: " ${VCPKG_SRC})
-  message(STATUS "VCPKG_ROOT: " $ENV{VCPKG_ROOT})
+  message(STATUS "ENV{VCPKG_ROOT}: " $ENV{VCPKG_ROOT})
 
+  # NOTE: The VCPKG_ROOT environment variable isn't propagated to an add_custom_command target, so specify --vcpkg-root
+  # here and in the vcpkg_install function
   add_custom_command(
-    COMMAND ${VCPKG_SRC}/vcpkg integrate install
+    COMMAND ${CMAKE_COMMAND} -E echo ${VCPKG_SRC}/vcpkg integrate --vcpkg-root=$ENV{VCPKG_ROOT} install
+    COMMAND ${VCPKG_SRC}/vcpkg integrate --vcpkg-root=$ENV{VCPKG_ROOT} install
     COMMAND ${CMAKE_COMMAND} -E touch vcpkg_integrate.stamp
     OUTPUT vcpkg_integrate.stamp
     DEPENDS vcpkg
@@ -34,76 +44,79 @@ if (WIN32)
   add_custom_target(vcpkg_integrate ALL DEPENDS vcpkg_integrate.stamp)
   set(VCPKG_DEPENDENCIES "vcpkg_integrate")
 
+  # use static-md so it adjusts for debug/release CRT
+  # https://stackoverflow.com/questions/67258905/vcpkg-difference-between-windows-windows-static-and-other
   function(vcpkg_install PACKAGE_NAME)
     add_custom_command(
-      OUTPUT ${VCPKG_SRC}/packages/${PACKAGE_NAME}_${vcpkg_target_platform}-windows-static/BUILD_INFO
-      COMMAND ${VCPKG_SRC}/vcpkg install ${PACKAGE_NAME}:${vcpkg_target_platform}-windows-static --vcpkg-root=${CMAKE_CURRENT_BINARY_DIR}/_deps/vcpkg-src
+      OUTPUT ${VCPKG_SRC}/packages/${PACKAGE_NAME}_${vcpkg_target_platform}-windows-static-md/BUILD_INFO
+      COMMAND ${CMAKE_COMMAND} -E echo ${VCPKG_SRC}/vcpkg install --vcpkg-root=$ENV{VCPKG_ROOT}
+                                  ${PACKAGE_NAME}:${vcpkg_target_platform}-windows-static-md
+      COMMAND ${VCPKG_SRC}/vcpkg install --vcpkg-root=$ENV{VCPKG_ROOT}
+                                         ${PACKAGE_NAME}:${vcpkg_target_platform}-windows-static-md
       WORKING_DIRECTORY ${VCPKG_SRC}
       DEPENDS vcpkg_integrate)
 
-    add_custom_target(get${PACKAGE_NAME}
+    add_custom_target(
+      get${PACKAGE_NAME}
       ALL
-      DEPENDS ${VCPKG_SRC}/packages/${PACKAGE_NAME}_${vcpkg_target_platform}-windows-static/BUILD_INFO)
+      DEPENDS ${VCPKG_SRC}/packages/${PACKAGE_NAME}_${vcpkg_target_platform}-windows-static-md/BUILD_INFO)
 
     list(APPEND VCPKG_DEPENDENCIES "get${PACKAGE_NAME}")
     set(VCPKG_DEPENDENCIES ${VCPKG_DEPENDENCIES} PARENT_SCOPE)
   endfunction()
 
-  vcpkg_install(openssl)
-  vcpkg_install(openssl-windows)
   vcpkg_install(rapidjson)
-  vcpkg_install(re2)
-  vcpkg_install(boost-interprocess)
-  vcpkg_install(boost-stacktrace)
-  vcpkg_install(pthread)
-  vcpkg_install(b64)
+  vcpkg_install(openssl)
   vcpkg_install(curl)
 
-  add_dependencies(getb64 getpthread)
-  add_dependencies(getpthread getboost-stacktrace)
-  add_dependencies(getboost-stacktrace getboost-interprocess)
-  add_dependencies(getboost-interprocess getre2)
-  add_dependencies(getre2 getrapidjson)
-  add_dependencies(getrapidjson getopenssl-windows)
-  add_dependencies(getopenssl-windows getopenssl)
+  set(triton_extra_cmake_args -DVCPKG_TARGET_TRIPLET=${vcpkg_target_platform}-windows-static-md
+                              -DCMAKE_TOOLCHAIN_FILE=${VCPKG_SRC}/scripts/buildsystems/vcpkg.cmake)
+  set(triton_dependencies ${VCPKG_DEPENDENCIES})
 
-  ExternalProject_Add(triton
-                      GIT_REPOSITORY https://github.com/triton-inference-server/client.git
-                      GIT_TAG r23.05
-                      PREFIX triton
-                      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/triton-src
-                      BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/triton-build
-                      CMAKE_ARGS -DVCPKG_TARGET_TRIPLET=${vcpkg_target_platform}-windows-static -DCMAKE_TOOLCHAIN_FILE=${VCPKG_SRC}/scripts/buildsystems/vcpkg.cmake -DCMAKE_INSTALL_PREFIX=binary -DTRITON_ENABLE_CC_HTTP=ON -DTRITON_ENABLE_ZLIB=OFF
-                      INSTALL_COMMAND ""
-                      UPDATE_COMMAND "")
-
-  add_dependencies(triton ${VCPKG_DEPENDENCIES})
 else()
-  ExternalProject_Add(curl7
-                      PREFIX curl7
-                      GIT_REPOSITORY "https://github.com/curl/curl.git"
-                      GIT_TAG "curl-7_86_0"
-                      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/curl7-src
-                      BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/curl7-build
-                      CMAKE_ARGS -DBUILD_TESTING=OFF -DBUILD_CURL_EXE=OFF -DBUILD_SHARED_LIBS=OFF -DCURL_STATICLIB=ON -DHTTP_ONLY=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
+  # RapidJSON 1.1.0 (released in 2016) is compatible with the triton build. Later code is not compatible without
+  # patching due to the change in variable name for the include dir from RAPIDJSON_INCLUDE_DIRS to
+  # RapidJSON_INCLUDE_DIRS in the generated cmake file used by find_package:
+  #   https://github.com/Tencent/rapidjson/commit/b91c515afea9f0ba6a81fc670889549d77c83db3
+  # The triton code here https://github.com/triton-inference-server/common/blob/main/CMakeLists.txt is using
+  # RAPIDJSON_INCLUDE_DIRS so the build fails if a newer RapidJSON version is used. It will find the package but the
+  # include path will be wrong so the build error is delayed/misleading and non-trivial to understand/resolve.
+  set(RapidJSON_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/_deps/rapidjson)
+  set(RapidJSON_INSTALL_DIR ${RapidJSON_PREFIX}/install)
+  ExternalProject_Add(RapidJSON
+                      PREFIX ${RapidJSON_PREFIX}
+                      URL https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.zip
+                      URL_HASH SHA1=0fe7b4f7b83df4b3d517f4a202f3a383af7a0818
+                      CMAKE_ARGS -DRAPIDJSON_BUILD_DOC=OFF
+                                 -DRAPIDJSON_BUILD_EXAMPLES=OFF
+                                 -DRAPIDJSON_BUILD_TESTS=OFF
+                                 -DRAPIDJSON_HAS_STDSTRING=ON
+                                 -DRAPIDJSON_USE_MEMBERSMAP=ON
+                                 -DCMAKE_INSTALL_PREFIX=${RapidJSON_INSTALL_DIR}
+                                 )
 
-  ExternalProject_Add(triton
-                      GIT_REPOSITORY https://github.com/triton-inference-server/client.git
-                      GIT_TAG r23.05
-                      PREFIX triton
-                      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/triton-src
-                      BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/triton-build
-                      CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=binary -DTRITON_ENABLE_CC_HTTP=ON -DTRITON_ENABLE_ZLIB=OFF
-                      INSTALL_COMMAND ""
-                      UPDATE_COMMAND "")
+  ExternalProject_Get_Property(RapidJSON SOURCE_DIR BINARY_DIR)
+  # message(STATUS "RapidJSON src=${SOURCE_DIR} binary=${BINARY_DIR}")
+  # Set RapidJSON_ROOT_DIR for find_package. The required RapidJSONConfig.cmake file is generated in the binary dir
+  set(RapidJSON_ROOT_DIR ${BINARY_DIR})
 
-  add_dependencies(triton curl7)
-
+  set(triton_extra_cmake_args "")
+  set(triton_dependencies RapidJSON)
 endif() #if (WIN32)
 
-ExternalProject_Get_Property(triton SOURCE_DIR)
-set(TRITON_SRC ${SOURCE_DIR})
+# Add the triton build. We just need the library so we don't install it.
+ExternalProject_Add(triton
+                    URL https://github.com/triton-inference-server/client/archive/refs/heads/r23.05.tar.gz
+                    URL_HASH SHA1=b8fd2a4e09eae39c33cd04cfa9ec934e39d9afc1
+                    PREFIX ${triton_PREFIX}
+                    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${triton_INSTALL_DIR}
+                               -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                               -DTRITON_ENABLE_CC_HTTP=ON
+                               -DTRITON_ENABLE_ZLIB=OFF
+                               ${triton_extra_cmake_args}
+                    CMAKE_CACHE_ARGS -DTRITON_USE_THIRD_PARTY:BOOL=OFF
+                    INSTALL_COMMAND cmake -E echo "Skipping install step.")
 
-ExternalProject_Get_Property(triton BINARY_DIR)
-set(TRITON_BIN ${BINARY_DIR}/binary)
-set(TRITON_THIRD_PARTY ${BINARY_DIR}/third-party)
+add_dependencies(triton ${triton_dependencies})
+
+set(triton_THIRD_PARTY_DIR ${BINARY_DIR}/third-party)
