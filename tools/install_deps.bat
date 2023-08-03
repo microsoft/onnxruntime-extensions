a0b5e736763fbb9d662a09562583ed53cb773097
Scott McKay <Scott.McKay@microsoft.com>
2023-08-03 11:27:26 +1000

    Add variable to Windows build to enable AzureEP ops Hardcode install_dep.bat to run for all builds temporarily

--- tools/install_deps.bat
+++ tools/install_deps.bat
@@ -1,11 +1,11 @@
 @echo off
 if "%1" == "install" (
-  if "%OCOS_ENABLE_AZURE%"=="1" (
+REM  if "%OCOS_ENABLE_AZURE%"=="1" (
     pip install cmake
     if not exist "%ProgramFiles%\Miniconda3\python3.exe" (
       mklink "%ProgramFiles%\Miniconda3\python3.exe" "%ProgramFiles%\Miniconda3\python.exe"
     )
-  )
+REM  )
 ) else (
   if "%OCOS_ENABLE_AZURE%"=="1" (
     pip uninstall cmake
