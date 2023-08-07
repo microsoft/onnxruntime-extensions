3128c50da247fb644998ad058fa60d74ee68621b
Wenbing Li <wenbingl@outlook.com>
2023-08-07 14:38:07 -0700

    More portable Java compiling support in MacOS

--- cmake/ext_java.cmake
+++ cmake/ext_java.cmake
@@ -2,6 +2,11 @@ include(FindJava)
 find_package(Java REQUIRED)
 include(UseJava)
 if (NOT ANDROID)
+    set(JAVA_AWT_LIBRARY NotNeeded)
+set(JAVA_JVM_LIBRARY NotNeeded)
+set(JAVA_INCLUDE_PATH2 NotNeeded)
+set(JAVA_AWT_INCLUDE_PATH NotNeeded)
+
     find_package(JNI REQUIRED)
 endif()
 
