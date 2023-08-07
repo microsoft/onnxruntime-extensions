80a18dd64bb54821abef980c93e07e37b96f9205
Randy Shuai <rashuai@microsoft.com>
2023-08-07 13:29:23 -0700

    install ort nightly

--- tools/test_cibuildwheel.bat
+++ tools/test_cibuildwheel.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%OCOS_ENABLE_AZURE%"=="1" (
     pushd %1\test
-    python -m pip install onnxruntime
+    python -m pip install -i https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple/ ort-nightly
     python test_azure_ops.py
     popd
 )
--- tools/test_cibuildwheel.sh
+++ tools/test_cibuildwheel.sh
@@ -3,7 +3,7 @@
 if [[ "$OCOS_ENABLE_AZURE" == "1" ]]
 then
     pushd $1/test
-    python -m pip install onnxruntime
+    python -m pip install -i https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple/ ort-nightly
     python ./test_azure_ops.py
     popd
 fi
