95012be825f09fae2007b995b7c37d5f913a9101
Scott McKay <skottmckay@gmail.com>
2023-08-15 07:52:23 +1000

    Disable warning for ignored attribute. Add `inline` to the full specialization of the Logger

--- operators/azure/logger.hpp
+++ operators/azure/logger.hpp
@@ -10,9 +10,20 @@ namespace ort_extensions {
 template <class, class = void>
 struct has_Logger_LogMessage : std::false_type {};
 
+// Disable GCC 'ignoring attributes on template argument' warning due to Logger_LogMessage using 
+// `__attribute__((warn_unused_result))`. 
+// The template here is about whether the function exists or not so the attribute is irrelevant.
+// FWIW using `[[nodiscard]]` would be fine, but that would require a change to the ORT_MUST_USE_RESULT
+// definition in onnxruntime_c_api.h.
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+#endif
 template <class T>
 struct has_Logger_LogMessage<T, std::void_t<decltype(&T::Logger_LogMessage)>> : std::true_type {};
-
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 // for use in implementation of classes that have a GetLogger() member that returns an ort_extensions::Logger<T>.
 // severity is an ORT_LOGGING_LEVEL_... value (e.g. ORT_LOGGING_LEVEL_WARNING)
 #define KERNEL_LOG(severity, msg) \
@@ -22,16 +33,16 @@ struct has_Logger_LogMessage<T, std::void_t<decltype(&T::Logger_LogMessage)>> : 
 template <typename T = std::false_type>
 class Logger {
  public:
-  Logger(const OrtApi& api, const OrtKernelInfo& /*info*/) : api_{api} {
+  Logger(const OrtApi& api, const OrtKernelInfo& /*info*/) noexcept : api_{api} {
   }
 
   void LogMessage(OrtLoggingLevel log_severity_level, const ORTCHAR_T* file_path, int line_number,
-                  const char* func_name, const char* message) const {
+                  const char* func_name, const char* message) const noexcept {
     DefaultLogMessage(log_severity_level, file_path, line_number, func_name, message);
   };
 
   static void DefaultLogMessage(OrtLoggingLevel log_severity_level, const ORTCHAR_T* file_path, int line_number,
-                                const char* func_name, const char* message) {
+                                const char* func_name, const char* message) noexcept {
     // hardcoded when using fallback due to old ORT version.
     if (log_severity_level >= ORT_LOGGING_LEVEL_WARNING) {
       OrtW::LogError(file_path, line_number, (std::string(func_name) + ": " + message).c_str());
@@ -48,8 +59,8 @@ class Logger {
 // specialization for when ORT logger is available (compiled against and running with ORT version > 1.15
 // return 'bool'. the actual type doesn't matter - just has to be different to the default of 'void'
 template <>
-Logger<has_Logger_LogMessage<OrtApi>::type>::Logger(const OrtApi& api,
-                                                    const OrtKernelInfo& info)
+inline Logger<has_Logger_LogMessage<OrtApi>::type>::Logger(const OrtApi& api,
+                                                    const OrtKernelInfo& info) noexcept
     : api_{api}, api_version_{GetActiveOrtAPIVersion()} {
   // Get logger from the OrtKernelInfo should never fail. The logger comes from the EP, and is set when the EP is
   // registered in the InferenceSession, which happens before model load.
@@ -60,9 +71,9 @@ Logger<has_Logger_LogMessage<OrtApi>::type>::Logger(const OrtApi& api,
 }
 
 template <>
-void Logger<has_Logger_LogMessage<OrtApi>::type>::LogMessage(
+inline void Logger<has_Logger_LogMessage<OrtApi>::type>::LogMessage(
     OrtLoggingLevel log_severity_level, const ORTCHAR_T* file_path, int line_number,
-    const char* func_name, const char* message) const {
+    const char* func_name, const char* message) const noexcept {
   // Logger_LogMessage was added in ORT 1.15
   if (api_version_ >= 15) {
     auto status = api_.Logger_LogMessage(static_cast<const OrtLogger*>(ort_logger_), log_severity_level, message,
