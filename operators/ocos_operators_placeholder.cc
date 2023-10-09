b1c3dd3974e00158642f45d0c296be915b6d0bac
Baiju Meswani <bmeswani@microsoft.com>
2023-10-09 13:50:00 -0700

    Ensure noexcep_operators and ocos_operators get built always

---
+++ operators/ocos_operators_placeholder.cc
@@ -0,0 +1,3 @@
+// Empty file that ensures that the ocos_operators lib is created.
+// Simplifies the setup when ORT is building with extensions and either
+// ocos_oeprators or noexcp_operators have empty source lists.
