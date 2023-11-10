73e69bb779c9548af764ddb5aaac8419236671fe
Rachel Guo <35738743+YUNQIUGUO@users.noreply.github.com>
2023-11-09 18:38:50 -0800

    Make `token_type_ids` optional output for HFBertTokenizer op (#583)
    * add initial changes for optional outpout for berttokenizer
    
    * add test tokenizer onnx model
    
    * update
    
    * fix
    
    * remove commented out code
    
    * fix extend list elements
    
    * fix
    
    * minor update
    
    * test
    
    * return optional in outputcharacteristic
    
    * update
    
    * update
    
    * debug
    
    * fix flag
    
    * remove print msg
    
    * minor update
    
    * update comments
    
    * update pr comments
    
    * minor update
    
    * minor update
    
    * fix
    
    ---------
    
    Co-authored-by: rachguo <rachguo@rachguos-Mac-mini.local>
    Co-authored-by: Sayan Shaw <52221015+sayanshaw24@users.noreply.github.com>
    Co-authored-by: rachguo <rachguo@rachguos-Mini.attlocal.net>
    Co-authored-by: Scott McKay <skottmckay@gmail.com>

--- test/shared_test/test_ortops_hfberttokenizer.cc
+++ test/shared_test/test_ortops_hfberttokenizer.cc
@@ -7,10 +7,9 @@
 #include "ocos.h"
 #include "test_kernel.hpp"
 
-TEST(hf_bert_tokenizer_opertor, test_default) {
+void TestHfBertTokenizerHelper(const std::filesystem::path& test_model_path,
+                               bool validate_optional_token_type_ids_output) {
   auto ort_env = std::make_unique<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "Default");
-
-  const std::filesystem::path model_path = "data/bert-large-uncased-whole-word-masking-finetuned-squad-tokenizer.onnx";
 
   const std::vector<int64_t> input_dims{2};
   const std::string context1 =
@@ -37,18 +36,13 @@ TEST(hf_bert_tokenizer_opertor, test_default) {
       {TestValue{"text", {"Which state does John's friend live?", context2}, input_dims}}};
 
   std::vector<std::vector<TestValue>> outputs = {
-      {TestValue{
-           "input_ids",
-           std::vector<int64_t>{101, 2040, 2003, 2198, 1005, 1055, 2905, 1029, 102, 2198, 2003, 1037, 2184, 2095, 2214,
-                                2879, 1012, 2002, 2003, 1996, 2365, 1997, 2728, 3044, 1012, 3870, 4482, 2003, 2728,
-                                1005, 1055, 2564, 1012, 2016, 12011, 2012, 15384, 8256, 1012, 9665, 3044, 2003, 3870,
-                                1005, 1055, 2684, 1012, 2016, 2913, 2012, 15384, 4482, 102},
-           {1, 53}},
-       TestValue{"attention_mask", std::vector<int64_t>(53, 1), {1, 53}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 53}}},
+      {TestValue{"input_ids",
+                 std::vector<int64_t>{101, 2040, 2003, 2198, 1005, 1055, 2905, 1029, 102, 2198, 2003, 1037, 2184, 2095, 2214,
+                                      2879, 1012, 2002, 2003, 1996, 2365, 1997, 2728, 3044, 1012, 3870, 4482, 2003, 2728,
+                                      1005, 1055, 2564, 1012, 2016, 12011, 2012, 15384, 8256, 1012, 9665, 3044, 2003, 3870,
+                                      1005, 1055, 2684, 1012, 2016, 2913, 2012, 15384, 4482, 102},
+                 {1, 53}},
+       TestValue{"attention_mask", std::vector<int64_t>(53, 1), {1, 53}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2073, 2515, 9665, 2817, 1029, 102, 2198, 2003, 1037, 2184, 2095, 2214, 2879,
@@ -56,11 +50,7 @@ TEST(hf_bert_tokenizer_opertor, test_default) {
                                       1005, 1055, 2564, 1012, 2016, 12011, 2012, 15384, 8256, 1012, 9665, 3044, 2003,
                                       3870, 1005, 1055, 2684, 1012, 2016, 2913, 2012, 15384, 4482, 102},
                  {1, 51}},
-       TestValue{"attention_mask", std::vector<int64_t>(51, 1), {1, 51}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 51}}},
+       TestValue{"attention_mask", std::vector<int64_t>(51, 1), {1, 51}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2040, 2003, 2198, 1005, 1055, 3566, 1029, 102, 2198, 2003, 1037, 2184, 2095,
@@ -68,11 +58,7 @@ TEST(hf_bert_tokenizer_opertor, test_default) {
                                       2003, 2728, 1005, 1055, 2564, 1012, 2016, 12011, 2012, 15384, 8256, 1012, 9665,
                                       3044, 2003, 3870, 1005, 1055, 2684, 1012, 2016, 2913, 2012, 15384, 4482, 102},
                  {1, 53}},
-       TestValue{"attention_mask", std::vector<int64_t>(53, 1), {1, 53}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 53}}},
+       TestValue{"attention_mask", std::vector<int64_t>(53, 1), {1, 53}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2073, 2515, 2198, 1005, 1055, 2269, 1005, 1055, 2564, 6570, 1029, 102, 2198,
@@ -81,12 +67,7 @@ TEST(hf_bert_tokenizer_opertor, test_default) {
                                       15384, 8256, 1012, 9665, 3044, 2003, 3870, 1005, 1055, 2684, 1012, 2016, 2913,
                                       2012, 15384, 4482, 102},
                  {1, 57}},
-       TestValue{"attention_mask", std::vector<int64_t>(57, 1), {1, 57}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1},
-                 {1, 57}}},
+       TestValue{"attention_mask", std::vector<int64_t>(57, 1), {1, 57}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2040, 2003, 2198, 1005, 1055, 2269, 1005, 1055, 2564, 1005, 1055, 2684,
@@ -95,49 +76,79 @@ TEST(hf_bert_tokenizer_opertor, test_default) {
                                       1055, 2564, 1012, 2016, 12011, 2012, 15384, 8256, 1012, 9665, 3044, 2003, 3870,
                                       1005, 1055, 2684, 1012, 2016, 2913, 2012, 15384, 4482, 102},
                  {1, 62}},
-       TestValue{"attention_mask", std::vector<int64_t>(62, 1), {1, 62}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 62}}},
+       TestValue{"attention_mask", std::vector<int64_t>(62, 1), {1, 62}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2040, 2003, 2198, 1005, 1055, 2767, 1029, 102, 2026, 2171, 2003, 2198, 1012,
                                       1045, 2444, 1999, 2624, 4560, 1010, 2662, 1012, 6487, 2003, 2026, 2767, 1012,
                                       2002, 3268, 1999, 5862, 1010, 2899, 1012, 102},
                  {1, 35}},
-       TestValue{"attention_mask", std::vector<int64_t>(35, 1), {1, 35}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 35}}},
+       TestValue{"attention_mask", std::vector<int64_t>(35, 1), {1, 35}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2073, 2515, 2198, 1005, 1055, 2767, 2444, 1029, 102, 2026, 2171, 2003, 2198,
                                       1012, 1045, 2444, 1999, 2624, 4560, 1010, 2662, 1012, 6487, 2003, 2026, 2767,
                                       1012, 2002, 3268, 1999, 5862, 1010, 2899, 1012, 102},
                  {1, 36}},
-       TestValue{"attention_mask", std::vector<int64_t>(36, 1), {1, 36}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 36}}},
+       TestValue{"attention_mask", std::vector<int64_t>(36, 1), {1, 36}}},
 
       {TestValue{"input_ids",
                  std::vector<int64_t>{101, 2029, 2110, 2515, 2198, 1005, 1055, 2767, 2444, 1029, 102, 2026, 2171, 2003,
                                       2198, 1012, 1045, 2444, 1999, 2624, 4560, 1010, 2662, 1012, 6487, 2003, 2026,
                                       2767, 1012, 2002, 3268, 1999, 5862, 1010, 2899, 1012, 102},
                  {1, 37}},
-       TestValue{"attention_mask", std::vector<int64_t>(37, 1LL), {1, 37}},
-       TestValue{"token_type_ids",
-                 std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                 {1, 37}}}};
+       TestValue{"attention_mask", std::vector<int64_t>(37, 1LL), {1, 37}}}};
+
+  if (validate_optional_token_type_ids_output) {
+    outputs[0].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 53}});
+    outputs[1].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 51}});
+    outputs[2].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 53}});
+    outputs[3].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1},
+                                   {1, 57}});
+    outputs[4].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 62}});
+    outputs[5].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 35}});
+    outputs[6].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 36}});
+    outputs[7].push_back(TestValue{"token_type_ids",
+                                   std::vector<int64_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                   {1, 37}});
+  }
 
   ASSERT_EQ(inputs.size(), outputs.size());
 
   for (size_t i = 0, count = inputs.size(); i < count; ++i) {
-    TestInference(*ort_env, model_path.c_str(), inputs[i], outputs[i]);
+    TestInference(*ort_env, test_model_path.c_str(), inputs[i], outputs[i]);
   }
+}
+
+TEST(hf_bert_tokenizer_operator, test_default) {
+  const std::filesystem::path model_path = "data/bert-large-uncased-whole-word-masking-finetuned-squad-tokenizer.onnx";
+  TestHfBertTokenizerHelper(model_path, true);
+}
+
+TEST(hf_bert_tokenizer_operator, test_optional_output) {
+  const std::filesystem::path model_path = "data/test_hf_bert_tokenizer_optional_output.onnx";
+  TestHfBertTokenizerHelper(model_path, false);
 }
