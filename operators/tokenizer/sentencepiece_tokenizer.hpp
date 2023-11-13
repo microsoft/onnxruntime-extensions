bf72ef78251aec8b22e1764570bbca10b0b6ce6f
Wenbing Li <wenbingl@outlook.com>
2023-11-13 11:40:27 -0800

    migrate the spm and trie tokenizer to falliable API

--- operators/tokenizer/sentencepiece_tokenizer.hpp
+++ operators/tokenizer/sentencepiece_tokenizer.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 #pragma once
@@ -7,18 +7,19 @@
 #include "string_utils.h"
 #include "sentencepiece_processor.h"
 
-struct KernelSentencepieceTokenizer : BaseKernel {
-  KernelSentencepieceTokenizer(const OrtApi& api, const OrtKernelInfo& info);
-  void Compute(const ortc::Tensor<std::string>& input,
-               int64_t /*nbest_size*/,
-               float /*alpha*/,
-               bool add_bos,
-               bool add_eos,
-               bool add_rev,
-               ortc::Tensor<int32_t>& output,
-               ortc::Tensor<int64_t>& output1,
-               std::optional<bool> fairseq,
-               std::optional<ortc::Tensor<int32_t>*> output2) const;
+struct KernelSentencepieceTokenizer {
+  OrtStatusPtr OnModelAttach(const OrtApi& api, const OrtKernelInfo& info);
+
+  OrtStatusPtr Compute(const ortc::Tensor<std::string>& input,
+                       int64_t /*nbest_size*/,
+                       float /*alpha*/,
+                       bool add_bos,
+                       bool add_eos,
+                       bool add_rev,
+                       ortc::Tensor<int32_t>& output,
+                       ortc::Tensor<int64_t>& output1,
+                       std::optional<bool> fairseq,
+                       std::optional<ortc::Tensor<int32_t>*> output2) const;
 
  private:
   sentencepiece::SentencePieceProcessor tokenizer_;
