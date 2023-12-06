8932234b4fc1924ff895be8f8d5f0899d9ae889c
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2023-12-05 16:02:04 -0800

    Update gen-ai_llama.py

--- tutorials/gen-ai_llama.py
+++ tutorials/gen-ai_llama.py
@@ -7,18 +7,17 @@
 
 import onnxruntime_genai as og
 import numpy as np
-from transformers import LlamaTokenizer
-from onnxruntime_extensions import OrtPyFunction, gen_processing_models
+import onnxruntime_extensions as _ortx
 
 device_type = og.DeviceType.CPU
 #device_type = og.DeviceType.CUDA
 
-# Generate input tokens from the text prompt
-tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
+# ort-tokenizer can be converted with following model
+# from transformers import LlamaTokenizer
+# tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
+# _ortx.gen_processing_models(
+#    tokenizer).save("../test_models/llama2-tokenizer.json")
 
-ort_tok = OrtPyFunction.from_model(gen_processing_models(
-    tokenizer,
-    pre_kwargs={"WITH_DEFAULT_INPUTS": True})[0])
 
 print("Loading model...")
 #model=og.Llama_Model("../../test_models/llama2-7b-fp32-cpu/Llama-2-7b-hf_decoder_merged_model_fp32_opt.onnx", device_type)
@@ -27,12 +26,12 @@ print("Loading model...")
 #model=og.Model("../../test_models/llama2-7b-chat-int4-gpu", device_type)
 model=og.Model("../../test_models/llama2-7b-fp32-cpu/", device_type)
 print("Model loaded")
+tokenizer = _ortx.LoadTokenizer("../test_models/llama2-tokenizer.json")
 
 # Keep asking for input prompts in an loop
 while True:
     text = input("Input:")
-    input_tokens = ort_tok([text])[0]
-    print("ORT Extensions Tokenized Input IDs: " + str(input_tokens))
+    input_tokens = tokenizer([text])[0]
 
     params=og.SearchParams(model)
     params.max_length = 128
@@ -47,18 +46,10 @@ while True:
     while not search.IsDone():
         search.SetLogits(state.Run(search.GetSequenceLength(), search.GetNextTokens()))
 
-        # search.Apply_MinLength(1)
-        # search.Apply_RepetitionPenalty(1.0)
+        search.Apply_MinLength(1)
+        search.Apply_RepetitionPenalty(1.0)
 
         search.SampleTopP(0.7, 0.6)
 
-        # Print each token as we compute it, we have to do some work to get newlines & spaces to appear properly:
-        word=tokenizer.convert_ids_to_tokens([search.GetNextTokens().GetArray()[0]])[0]
-        if word=='<0x0A>':
-          word = '\n'
-        if word.startswith('▁'):
-          word = ' ' + word[1:]
-        print(word, end='', flush=True)
-
-    print()
-    print()
+    decoded_text = tokenizer.Detokenizer(search.GetSequence(0))
+    print(decoded_text)
