dce0d9c72c00a6b28fd1676fc66f3e21bbcbf5f2
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2023-12-08 10:19:54 -0800

    Add the missing tasks and fix the issues in CUDA python unit test pipelines (#615)
    * add an experimental CUDA python unit test pipeline
    
    * typo
    
    * in ci.yml?
    
    * winpycuda
    
    * move it in optional
    
    * enable cuda pytest in linuxbuild
    
    * build in docker
    
    * add the cuda pytest for windows
    
    * cuda flag fixing
    
    * minor fixing
    
    * typo
    
    ---------
    
    Co-authored-by: Yi Zhang <zhanyi@microsoft.com>

--- .pyproject/backend.py
+++ .pyproject/backend.py
@@ -7,7 +7,7 @@
 import os
 import sys
 from setuptools import build_meta as _orig
-from setuptools.build_meta import *  # noqa: F403
+from setuptools.build_meta import *  # noqa: F401, F403
 
 # add the current directory to the path, so we can import setup_cmds.py
 sys.path.append(os.path.dirname(__file__))
