386515d3be93c1da87e414c6290f75dabc3ca2e8
Wenbing Li <wenbingl@outlook.com>
2024-01-24 22:41:16 +0000

    the new tokenizer API

---
+++ cmake/externals/simdjson.cmake
@@ -0,0 +1,8 @@
+FetchContent_Declare(
+    simdjson
+    URL https://github.com/simdjson/simdjson/archive/refs/tags/v3.6.3.zip
+    URL_HASH SHA1=2b063a2e81f74a5d1cb937fadf3d2fca0f1edb09
+  )
+
+  FetchContent_MakeAvailable(simdjson)
+  
