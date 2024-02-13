1061da3e44eca4576b803086165e873c64cfbc29
Wenbing Li <wenbingl@outlook.com>
2024-02-13 00:50:17 +0000

    remove the simdjson

--- cmake/externals/simdjson.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(
-    simdjson
-    URL https://github.com/simdjson/simdjson/archive/refs/tags/v3.6.3.zip
-    URL_HASH SHA1=2b063a2e81f74a5d1cb937fadf3d2fca0f1edb09
-  )
-
-FetchContent_MakeAvailable(simdjson)
