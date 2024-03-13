99cca01cf5c78c28854c3b1084a75ea7bd5819a2
Wenbing Li <wenbingl@outlook.com>
2024-03-13 21:48:34 +0000

    apply multi-archs more places

--- .pyproject/cmdclass.py
+++ .pyproject/cmdclass.py
@@ -38,7 +38,9 @@ def _load_cuda_version():
 
 def _load_nvidia_smi():
     try:
-        output = subprocess.check_output(["nvidia-smi", "--query-gpu=compute_cap", "--format=csv,noheader,nounits"], stderr=subprocess.STDOUT).decode("utf-8")
+        output = subprocess.check_output(
+            ["nvidia-smi", "--query-gpu=compute_cap", "--format=csv,noheader,nounits"],
+            stderr=subprocess.STDOUT).decode("utf-8")
         arch = output.strip().replace('.', '')
         return arch if arch.isdigit() else None
     except (subprocess.CalledProcessError, OSError):
