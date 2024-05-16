808c6e76fc15e027966e0d03a9fa40417d089c2c
Wenbing Li <wenbingl@outlook.com>
2024-05-16 10:03:50 -0700

    add other two pipelines

--- base/noexcep_operators_placeholder.cc
+++
@@ -1,3 +0,0 @@
-// Empty file that ensures that the noexcep_operators lib is created.
-// Simplifies the setup when ORT is building with extensions and either
-// ocos_oeprators or noexcp_operators have empty source lists.
