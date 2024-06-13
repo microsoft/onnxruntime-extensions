b8bad3fb16f3c2e063370ab59b27816a7b501efa
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2024-06-13 10:07:20 -0700

    add the missed files

--- tools/test_cibuildwheel.bat
+++ tools/test_cibuildwheel.bat
@@ -2,7 +2,7 @@
 if "%OCOS_ENABLE_AZURE%"=="1" (
     pushd %1\test
     python -m pip install coloredlogs flatbuffers numpy packaging protobuf sympy
-    python -m pip install -i https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple/ ort-nightly==1.16.0.dev20230820005
+    python -m pip install onnxruntime==1.18
     python test_azure_ops.py
     popd
 )
