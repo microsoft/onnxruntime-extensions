e4087ad2dc4c15751b9350190009cc3d5caf640c
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2024-07-15 15:19:02 -0700

    Update cutlass.cmake

--- cmake/externals/cutlass.cmake
+++ cmake/externals/cutlass.cmake
@@ -3,5 +3,5 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git
   GIT_TAG v3.1.0
   EXCLUDE_FROM_ALL)
-
+set(CUTLASS_ENABLE_HEADERS_ONLY ON CACHE BOOL "")
 FetchContent_MakeAvailable(cutlass)
