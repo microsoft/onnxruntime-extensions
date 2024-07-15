d2c3f2238721cae55b5aca002ccb513600ad08a3
Wenbing Li <10278425+wenbingl@users.noreply.github.com>
2024-07-15 11:49:02 -0700

    exclude cutlass too

--- cmake/externals/cutlass.cmake
+++ cmake/externals/cutlass.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
-        cutlass
-        GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git
-        GIT_TAG v3.1.0
-)
+  cutlass
+  GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git
+  GIT_TAG v3.1.0
+  EXCLUDE_FROM_ALL)
 
 FetchContent_MakeAvailable(cutlass)
