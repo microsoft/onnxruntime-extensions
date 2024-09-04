5c6efd1e721759c1e7c553ba003d827c3bb1e2d8
Wenbing Li <wenbingl@outlook.com>
2024-09-04 17:51:45 +0000

    avoid the codecs library conflicts

--- test/data/processor/image_to_numpy.py
+++ test/data/processor/image_to_numpy.py
@@ -28,13 +28,14 @@ img_proc = ImageProcessor(R"""
   }
 }""")
 
-result = img_proc.pre_process(os.path.dirname(__file__) + "/standard_s.jpg")
+img_name = "australia.jpg"
+result = img_proc.pre_process(os.path.dirname(__file__) + "/" + img_name)
 np_img = img_proc.to_numpy(result)
 print(np_img.shape, np_img.dtype)
 
 # can save the image back to disk
 img_rgb = np_img[0]
 img_bgr = img_rgb[..., ::-1]
-output_name = tempfile.gettempdir() + "/standard_s_bgr.jpg"
+output_name = tempfile.gettempdir() + "/" + img_name
 Image.fromarray(img_bgr).save(output_name)
 print(output_name)
