c3e8c15edffc7ceb7fe04ecc578543a93a96c083
Scott McKay <Scott.McKay@microsoft.com>
2024-09-09 10:45:26 +1000

    Don't disable vision operators in a catalyst build.

--- tools/build.py
+++ tools/build.py
@@ -534,7 +534,6 @@ def _generate_build_tree(
                     f"-DCMAKE_CC_FLAGS=--target={macabi_target}",
                     f"-DCMAKE_CC_FLAGS_RELEASE=-O3 -DNDEBUG --target={macabi_target}",
                     "-DOCOS_ENABLE_CV2=OFF",
-                    "-DOCOS_ENABLE_VISION=OFF",
                     "-DOCOS_ENABLE_CTESTS=OFF",
                     "-DMAC_CATALYST=1",
                 ]
