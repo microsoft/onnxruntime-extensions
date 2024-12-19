02e098ce00c753b53a096268f1cc027a861fd5f3
Chester Liu <4710575+skyline75489@users.noreply.github.com>
2024-12-19 16:47:20 +0800

    Update dr_libs for better ARM64EC support (#861)
    * Update dr_libs for better ARM64EC support
    
    * cgmanifest.json

--- cmake/externals/dr_libs.cmake
+++ cmake/externals/dr_libs.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(dr_libs
-    URL         https://github.com/mackron/dr_libs/archive/dbbd08d81fd2b084c5ae931531871d0c5fd83b87.zip
-    URL_HASH    SHA1=84a2a31ef890b6204223b12f71d6e701c0edcd92
+    URL         https://github.com/mackron/dr_libs/archive/660795b2834aebb2217c9849d668b6e4bd4ef810.zip
+    URL_HASH    SHA1=af554b21dcd1ab3c7c8d946638682a2cbccf3e16
     SOURCE_SUBDIR not_set
 )
 
