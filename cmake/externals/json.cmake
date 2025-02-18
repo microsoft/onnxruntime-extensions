4a0e1c0f0d4da1b7847f372acdeed0a53a91cfab
Changming Sun <chasun@microsoft.com>
2025-02-18 11:38:54 -0800

    update

--- cmake/externals/json.cmake
+++ cmake/externals/json.cmake
@@ -2,6 +2,8 @@ FetchContent_Declare(nlohmann_json
   URL       https://codeload.github.com/nlohmann/json/zip/refs/tags/v3.11.3
   URL_HASH  SHA1=5e88795165cc8590138d1f47ce94ee567b85b4d6
   SOURCE_SUBDIR not_set
+  EXCLUDE_FROM_ALL
+  FIND_PACKAGE_ARGS NAMES nlohmann_json
   )
 
 add_compile_definitions(JSON_HAS_CPP_17=1)
