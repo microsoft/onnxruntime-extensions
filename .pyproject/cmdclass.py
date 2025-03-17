fd5c44d778881819d27d141298d44fdfc8da934f
Wenbing Li <wenbingl@outlook.com>
2025-03-17 22:29:57 +0000

    undo cc-api enabling for python package.

--- .pyproject/cmdclass.py
+++ .pyproject/cmdclass.py
@@ -197,7 +197,6 @@ class CmdBuildCMakeExt(_build_ext):
             str(ext_fullpath.parent.absolute()),
             '-DOCOS_ENABLE_CTEST=OFF',
             '-DOCOS_BUILD_PYTHON=ON',
-            '-DOCOS_ENABLE_C_API=ON',
             '-DOCOS_PYTHON_MODULE_PATH=' + str(ext_fullpath),
             '-DCMAKE_BUILD_TYPE=' + config
         ]
@@ -215,7 +214,7 @@ class CmdBuildCMakeExt(_build_ext):
             if not self.no_opencv:
                 raise RuntimeError(
                     "Cannot enable PP C API Python Wrapper without disabling OpenCV.")
-            # cmake_args += ['-DOCOS_ENABLE_C_API=ON'], already set above
+            cmake_args += ['-DOCOS_ENABLE_C_API=ON']
 
         if self.no_azure is not None:
             azure_flag = "OFF" if self.no_azure == 1 else "ON"
