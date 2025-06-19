f936ba4be1917a670c73df2953b36093e4b16801
Sayan Shaw <52221015+sayanshaw24@users.noreply.github.com>
2025-06-19 13:16:06 -0700

    add chat_template.json and .jinja loading support (#968)
    Co-authored-by: Sayan Shaw <sayanshaw@microsoft.com>

--- test/pp_api_test/test_tokenizer_chat.cc
+++ test/pp_api_test/test_tokenizer_chat.cc
@@ -81,6 +81,92 @@ TEST(OrtxTokenizerTest, Phi4ChatTemplate) {
   ASSERT_EQ(std::string(text), expected_decoder_output);
 }
 
+/*
+
+Test loading chat template from chat_template.jinja file. The tokenizer_config.json from
+the model files in data/phi-4-mini-reasoning has been modified and the "chat_template" attribute
+has been removed from it. This is because the HuggingFace Transformers standard has changed and
+the chat template is now stored as a separate file, not as a key in tokenizer_config.json, so if
+a user calls load_pretrained or save_pretrained in the HF Python API, after loading a tokenizer, e.g.:
+
+tokenizer = AutoTokenizer.from_pretrained(input_folder)
+tokenizer.save_pretrained(output_folder)
+
+There will be a difference in the tokenizer_config.json files, removing the chat_template key,
+and adding a separate chat_template.jinja file with its value. This logic is now handled in
+our backend, and tested below.
+
+*/
+
+TEST(OrtxTokenizerTest, Phi4MiniReasoningChatTemplate) {
+  OrtxObjectPtr<OrtxTokenizer> tokenizer(OrtxCreateTokenizer, "data/phi-4-mini-reasoning");
+  ASSERT_EQ(tokenizer.Code(), kOrtxOK) << "Failed to create tokenizer, stopping the test.";
+
+  OrtxObjectPtr<OrtxTensorResult> templated_text;
+  std::string messages_json = R"(
+    [
+      {
+        "role": "system",
+        "content": "You are a medieval knight and must provide explanations to modern people."
+      },
+      {
+        "role": "user",
+        "content": "How should I explain the Internet?"
+      }
+    ])";
+
+  auto err = OrtxApplyChatTemplate(
+    tokenizer.get(), nullptr,
+    messages_json.c_str(), nullptr, templated_text.ToBeAssigned(), true, false);
+
+  if (err != kOrtxOK) {
+    std::cout << "Failed to apply chat template, stopping the test." << std::endl;
+    std::cout << "Error code: " << err << std::endl;
+    std::cout << "Error message: " << OrtxGetLastErrorMessage() << std::endl;
+  }
+
+  OrtxObjectPtr<OrtxTensor> tensor;
+  err = OrtxTensorResultGetAt(templated_text.get(), 0, tensor.ToBeAssigned());
+  ASSERT_EQ(tensor.Code(), kOrtxOK) << "Failed to get tensor from templated text, stopping the test.";
+  const char* text_ptr = nullptr;
+  OrtxGetTensorData(tensor.get(), reinterpret_cast<const void**>(&text_ptr), nullptr, nullptr);
+
+  // From HuggingFace Python output for 'microsoft/Phi-4-mini-reasoning'
+  std::string expected_output = "<|system|>Your name is Phi, an AI math expert developed by Microsoft. "
+                                "You are a medieval knight and must provide explanations to modern people."
+                                "<|end|><|user|>How should I explain the Internet?<|end|><|assistant|>";
+
+  ASSERT_EQ(text_ptr, expected_output);
+
+  OrtxObjectPtr<OrtxTokenId2DArray> token_ids;
+  const char* input[] = { text_ptr };
+  OrtxTokenize(tokenizer.get(), input, 1, token_ids.ToBeAssigned());
+  ASSERT_EQ(token_ids.Code(), kOrtxOK);
+
+  size_t length = 0;
+  const extTokenId_t* ids = nullptr;
+  OrtxTokenId2DArrayGetItem(token_ids.get(), 0, &ids, &length);
+  std::vector<extTokenId_t> ids_vec(ids, ids + length);
+
+  EXPECT_EQ(ids_vec, std::vector<extTokenId_t>({200022, 9719, 1308, 382, 102143, 11, 448, 20837, 13324, 8333,
+                                                9742, 656, 8321, 13, 1608, 553, 261, 55145, 105457, 326,
+                                                2804, 3587, 69457, 316, 6809, 1665, 13, 200020, 200021, 5299,
+                                                1757, 357, 16644, 290, 7380, 30, 200020, 200019}));
+  
+  OrtxObjectPtr<OrtxStringArray> decoded_text;
+  OrtxDetokenize(tokenizer.get(), token_ids.get(), decoded_text.ToBeAssigned());
+  EXPECT_EQ(decoded_text.Code(), kOrtxOK);
+
+  const char* text = nullptr;
+  OrtxStringArrayGetItem(decoded_text.get(), 0, &text);
+
+  std::string expected_decoder_output = "Your name is Phi, an AI math expert developed by Microsoft. "
+                                        "You are a medieval knight and must provide explanations to modern people."
+                                        "How should I explain the Internet?";
+
+  ASSERT_EQ(std::string(text), expected_decoder_output);
+}
+
 TEST(OrtxTokenizerTest, Phi3MediumChatTemplate) {
   OrtxObjectPtr<OrtxTokenizer> tokenizer(OrtxCreateTokenizer, "data/phi-3-medium");
   ASSERT_EQ(tokenizer.Code(), kOrtxOK) << "Failed to create tokenizer, stopping the test.";
@@ -344,6 +430,83 @@ TEST(OrtxTokenizerTest, Gemma3ChatTemplate) {
 
   EXPECT_EQ(ids_vec, std::vector<extTokenId_t>({2, 105, 2364, 107, 255999, 3689, 563, 506, 8918, 236881, 106, 107, 105, 4368, 107}));
   
+  OrtxObjectPtr<OrtxStringArray> decoded_text;
+  OrtxDetokenize(tokenizer.get(), token_ids.get(), decoded_text.ToBeAssigned());
+  EXPECT_EQ(decoded_text.Code(), kOrtxOK);
+
+  const char* text = nullptr;
+  OrtxStringArrayGetItem(decoded_text.get(), 0, &text);
+
+  std::string expected_decoder_output = "user\nWhat is the password?\nmodel\n";
+
+  ASSERT_EQ(std::string(text), expected_decoder_output);
+}
+
+/*
+
+Similar to the Phi4MiniReasoningChatTemplate test, this test checks for chat template loading
+from a separate chat_template.json file, instead of a key in tokenizer_config.json. Although
+the official HuggingFace model files for the gemma-3 model contain both the chat_template.json
+file as well as the key in tokenizer_config.json, we have removed the key in "data/gemma-3-chat"
+to test the special loading from the chat_template.json file.
+
+*/
+
+TEST(OrtxTokenizerTest, Gemma3SpecialChatTemplate) {
+  OrtxObjectPtr<OrtxTokenizer> tokenizer(OrtxCreateTokenizer, "data/gemma-3-chat");
+  ASSERT_EQ(tokenizer.Code(), kOrtxOK) << "Failed to create tokenizer, stopping the test.";
+
+  OrtxObjectPtr<OrtxTensorResult> templated_text;
+  std::string messages_json = R"(
+    [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "image",
+            "url": "https://huggingface.co/spaces/big-vision/paligemma-hf/resolve/main/examples/password.jpg"
+          },
+          {
+            "type": "text",
+            "text": "What is the password?"
+          }
+        ]
+      }
+    ])";
+
+  auto err = OrtxApplyChatTemplate(
+    tokenizer.get(), nullptr,
+    messages_json.c_str(), nullptr, templated_text.ToBeAssigned(), true, false);
+
+  if (err != kOrtxOK) {
+    std::cout << "Failed to apply chat template, stopping the test." << std::endl;
+    std::cout << "Error code: " << err << std::endl;
+    std::cout << "Error message: " << OrtxGetLastErrorMessage() << std::endl;
+  }
+
+  OrtxObjectPtr<OrtxTensor> tensor;
+  err = OrtxTensorResultGetAt(templated_text.get(), 0, tensor.ToBeAssigned());
+  ASSERT_EQ(tensor.Code(), kOrtxOK) << "Failed to get tensor from templated text, stopping the test.";
+  const char* text_ptr = nullptr;
+  OrtxGetTensorData(tensor.get(), reinterpret_cast<const void**>(&text_ptr), nullptr, nullptr);
+
+  // From HuggingFace Python output for 'google/gemma-3-4b-it'
+  std::string expected_output = "<bos><start_of_turn>user\n<start_of_image>What is the password?<end_of_turn>\n<start_of_turn>model\n";
+
+  ASSERT_EQ(text_ptr, expected_output);
+
+  OrtxObjectPtr<OrtxTokenId2DArray> token_ids;
+  const char* input[] = { text_ptr };
+  OrtxTokenizeWithOptions(tokenizer.get(), input, 1, token_ids.ToBeAssigned(), false);
+  ASSERT_EQ(token_ids.Code(), kOrtxOK);
+
+  size_t length = 0;
+  const extTokenId_t* ids = nullptr;
+  OrtxTokenId2DArrayGetItem(token_ids.get(), 0, &ids, &length);
+  std::vector<extTokenId_t> ids_vec(ids, ids + length);
+
+  EXPECT_EQ(ids_vec, std::vector<extTokenId_t>({2, 105, 2364, 107, 255999, 3689, 563, 506, 8918, 236881, 106, 107, 105, 4368, 107}));
+
   OrtxObjectPtr<OrtxStringArray> decoded_text;
   OrtxDetokenize(tokenizer.get(), token_ids.get(), decoded_text.ToBeAssigned());
   EXPECT_EQ(decoded_text.Code(), kOrtxOK);
