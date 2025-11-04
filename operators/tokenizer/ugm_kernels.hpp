3132c5b65aee74fb5db0be8e2fed7086f6f5319a
Akshay Sonawane <asonawane@microsoft.com>
2025-11-03 20:28:19 -0800

    Fix main

--- operators/tokenizer/ugm_kernels.hpp
+++ operators/tokenizer/ugm_kernels.hpp
@@ -773,12 +773,13 @@ class SpmUgmDecoder {
 
     std::vector<std::string> decoded_strings;
     decoded_strings.reserve(string_batch);
+    bool skip_special_tokens = add_special_tokens.value_or(true);
     TokenizerDecodingState* state{};
     for (auto n = string_batch; n > 0; n--) {
       std::string text;
       for (int64_t i = 0; i < seq_len; ++i) {
         std::string token;
-        Id2Token(ort_extensions::narrow<extTokenId_t>(p_ids[i]), token, &state);
+        Id2Token(ort_extensions::narrow<extTokenId_t>(p_ids[i]), token, &state, skip_special_tokens);
         text += token;
       }
 
