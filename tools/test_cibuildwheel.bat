748725b953f89a895349014b41b4770e691b952a
Sayan Shaw <52221015+sayanshaw24@users.noreply.github.com>
2026-04-03 10:08:19 -0700

    remove Azure custom ops (#1042)
    Co-authored-by: Sayan Shaw <sayanshaw@microsoft.com>

--- tools/test_cibuildwheel.bat
+++ tools/test_cibuildwheel.bat
@@ -1,8 +1,1 @@
 @echo off
-if "%OCOS_ENABLE_AZURE%"=="1" (
-    pushd %1\test
-    python -m pip install coloredlogs flatbuffers numpy packaging protobuf sympy
-    python -m pip install onnxruntime==1.18
-    python test_azure_ops.py
-    popd
-)
