8b05366741005b35b45eea0b66aaf410dabbe47b
Jain, Vishal <vishal.jain@amd.com>
2026-04-23 14:56:28 +0530

    Add modification notice

--- operators/tokenizer/bpe_tokenizer_model.hpp
+++ operators/tokenizer/bpe_tokenizer_model.hpp
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// Modifications Copyright(C) 2026 Advanced Micro Devices, Inc. All rights reserved
+
 #pragma once
 #include "ocos.h"
 #include "narrow.h"
