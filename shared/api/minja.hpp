7224899746bce83d62680f44f76579b9f24a80d1
Baiju Meswani <bmeswani@microsoft.com>
2026-09-15 06:33:19 +0000

    Support Jinja is true and is false predicates in chat templates

--- shared/api/minja.hpp
+++ shared/api/minja.hpp
@@ -1898,6 +1898,10 @@ namespace minja
             const auto &name = t->get_name();
             if (name == "none")
               return l.is_null() && !l.is_undefined();
+            if (name == "true")
+              return l.is_boolean() && l.get<bool>();
+            if (name == "false")
+              return l.is_boolean() && !l.get<bool>();
             if (name == "boolean")
               return l.is_boolean();
             if (name == "integer")
